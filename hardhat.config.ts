8e5316967fb26b4a872411880e15ef918855ccae
Jaxcoder <codenamejason@outlook.com>
2023-06-14 13:41:57 -0400

    adding hardhat/foundry empty project

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import "@nomicfoundation/hardhat-foundry";
+import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+};
+
+export default config;
