060c1545016ac2b09f6d8f8d3b0582545260c458
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-06-22 18:48:39 +0530

    chore: cleanup (#23)
    * chore: remove redundant comments
    
    * remove splitpayout
    
    * remove autogenerated docs
    
    * chore: remove tests + deploy scripts
    
    * chore: update hardhat config to remove redundant config
    
    * cleanup .env.sample file
    
    * chore: update hardhat config
    
    * chore: add prettier script + remove extra scripts
    
    * chore: recreate Registry based on interface
    
    * chore: address feedback commit
    
    Co-authored-by: codenamejason <codenamejason@outlook.com>
    
    ---------
    
    Co-authored-by: codenamejason <codenamejason@outlook.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,6 @@ import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-solhint";
 import "@nomiclabs/hardhat-waffle";
 import "@openzeppelin/hardhat-upgrades";
-import "@primitivefi/hardhat-dodoc";
 import "@typechain/hardhat";
 import "hardhat-abi-exporter";
 import "hardhat-contract-sizer";
@@ -19,14 +18,12 @@ dotenv.config();
 const chainIds = {
   // local
   localhost: 31337,
+
   // testnet
-  goerli: 5,
-  "fantom-testnet": 4002,
+  sepolia: 11155111,
 
   // mainnet
-  mainnet: 1,
-  "optimism-mainnet": 10,
-  "fantom-mainnet": 250,
+  mainnet: 1
 };
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -39,14 +36,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-let deployPrivateKey = process.env.DEPLOYER_PRIVATE_KEY as string;
-if (!deployPrivateKey) {
-  deployPrivateKey =
-    "0x0000000000000000000000000000000000000000000000000000000000000001";
-}
-
-const infuraIdKey = process.env.INFURA_ID as string;
-
 /**
  * Generates hardhat network configuration the test networks.
  * @param network
@@ -55,15 +44,11 @@ const infuraIdKey = process.env.INFURA_ID as string;
  */
 function createTestnetConfig(
   network: keyof typeof chainIds,
-  url?: string
+  url: string
 ): NetworkUserConfig {
-  if (!url) {
-    url = `https://${network}.infura.io/v3/${infuraIdKey}`;
-  }
   return {
-    accounts: [deployPrivateKey],
+    accounts: [process.env.DEPLOYER_PRIVATE_KEY as string],
     chainId: chainIds[network],
-    allowUnlimitedContractSize: true,
     url,
   };
 }
@@ -76,13 +61,10 @@ function createTestnetConfig(
  */
 function createMainnetConfig(
   network: keyof typeof chainIds,
-  url?: string
+  url: string
 ): NetworkUserConfig {
-  if (!url) {
-    url = `https://${network}.infura.io/v3/${infuraIdKey}`;
-  }
   return {
-    accounts: [deployPrivateKey],
+    accounts: [process.env.DEPLOYER_PRIVATE_KEY as string],
     chainId: chainIds[network],
     url,
   };
@@ -102,12 +84,6 @@ const abiExporter = [
   },
 ];
 
-const dodoc = {
-  outputDir: "./docs/generated",
-  exclude: ["contracts/dummy", "contracts/mocks"],
-  template: "markdown",
-};
-
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.19",
@@ -123,47 +99,44 @@ const config: HardhatUserConfig = {
     tests: "./test/hardhat",
   },
   networks: {
-    // Main Networks
-    mainnet: createMainnetConfig("mainnet"),
-    "optimism-mainnet": createMainnetConfig("optimism-mainnet"),
-    "fantom-mainnet": createMainnetConfig(
-      "fantom-mainnet",
-      "https://rpc.ftm.tools"
+    // ===============================
+    // ====== Mainnet Networks =======
+    // ===============================
+    mainnet: createMainnetConfig(
+      "mainnet",
+      process.env.MAINNET_RPC_URL as string
     ),
 
-    // Test Networks
-    goerli: createTestnetConfig("goerli"),
-    "fantom-testnet": createTestnetConfig(
-      "fantom-testnet",
-      "https://rpc.testnet.fantom.network/"
+    // ===============================
+    // ======== Test Networks ========
+    // ===============================
+    sepolia: createTestnetConfig(
+      "sepolia",
+      process.env.SEPOLIA_RPC_URL as string
     ),
-    localhost: createTestnetConfig("localhost", "http://127.0.0.1:8545"),
+
+    // Localhost
+    localhost: createTestnetConfig(
+      "localhost",
+      "http://127.0.0.1:8545"
+    ),
   },
+
   gasReporter: {
     coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-    excludeContracts: [
-      "contracts/mocks",
-      "contracts/dummy"
-    ],
+    currency: "USD"
   },
+
   etherscan: {
     apiKey: {
       // @ts-ignore
       mainnet: process.env.ETHERSCAN_API_KEY,
       // @ts-ignore
-      goerli: process.env.ETHERSCAN_API_KEY,
-      // @ts-ignore
-      optimisticEthereum: process.env.OPTIMISTIC_ETHERSCAN_API_KEY,
-      // @ts-ignore
-      ftmTestnet: process.env.FTMSCAN_API_KEY,
-      // @ts-ignore
-      opera: process.env.FTMSCAN_API_KEY,
+      sepolia: process.env.ETHERSCAN_API_KEY,
     },
   },
   abiExporter: abiExporter,
-  dodoc: dodoc,
 };
 
 export default config;
