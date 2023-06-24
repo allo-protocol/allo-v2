a21ef817f30edaa08fd0b331407d90fac0afd186
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-06-25 01:26:28 +0530

    chore: update Allo.sol

--- contracts/interfaces/IDistributionStrategy.sol
+++ contracts/interfaces/IDistributionStrategy.sol
@@ -13,12 +13,15 @@ interface IDistributionStrategy {
 
     // decode the _data into what's relevant to determine payouts
     // default will be a struct with a list of addresses and WAD percentages
-    // turn "on" the abilty to claim payouts
+    // turn "on" the abilty to distribute payouts
     function activateDistribution(bytes memory _inputData, bytes memory _allocStratData) external;
 
-    // claim a payout based on the strategy's needs
+    // distribution a payout based on the strategy's needs
     // this could include merkle proofs, etc or just nothing
-    function distribute(bytes memory _data) external;
+    function distribute(bytes memory _data, address sender) external;
+
+    /// close the distribution strategy. msg.sender should be Allo.sol
+    function close() external;
 
     // many owners will probably want a way to update roots, pull out funds if not claimed, etc
     // but all of that will be in specific implementations, not requried interface
