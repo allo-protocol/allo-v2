ac5484e31bb67583560a430add9b143899a0bec1
Jaxcoder <codenamejason@outlook.com>
2023-07-07 08:35:11 -0400

    chore: fmt/fix test deps

--- test/foundry/strategies/allocation/AllocationWithVotingBase.t.sol
+++ test/foundry/strategies/allocation/AllocationWithVotingBase.t.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.19;
 import "forge-std/Test.sol";
 
 import "../../../../contracts/interfaces/IAllocationStrategy.sol";
+import {Metadata} from "../../../../contracts/core/libraries/Metadata.sol";
+import {BaseAllocationStrategy} from "../../../../contracts/strategies/allocation/BaseAllocationStrategy.sol";
 
 contract AllocationWithVotingBaseTest is Test {
     uint256 public applicationStartTime;
@@ -12,11 +14,12 @@ contract AllocationWithVotingBaseTest is Test {
 
     bool public identityRequired;
 
-
     event ApplicationSubmitted(
         uint256 indexed applicationId, bytes32 indexed identityId, address recipient, Metadata metadata, address sender
     );
-    event ApplicationStatusUpdated(address indexed applicant, uint256 indexed applicationId, Status status);
+    event ApplicationStatusUpdated(
+        address indexed applicant, uint256 indexed applicationId, BaseAllocationStrategy.Status status
+    );
     event Allocated(bytes data, address indexed allocator);
     event Claimed(uint256 indexed applicationId, address receipient, uint256 amount);
     event TimestampsUpdated(
@@ -31,79 +34,49 @@ contract AllocationWithVotingBaseTest is Test {
         identityRequired = false;
     }
 
-    function test_initialize() public {
-    }
+    function test_initialize() public {}
 
-    function test_applyToPool_new_application() public {
-    }
+    function test_applyToPool_new_application() public {}
 
-    function test_applyToPool_update_application() public {
-    }
+    function test_applyToPool_update_application() public {}
 
-    function testRevert_applyToPool_APPLICATIONS_NOT_OPEN() public {
-    }
+    function testRevert_applyToPool_APPLICATIONS_NOT_OPEN() public {}
 
-    function testRevert_applyToPool_new_application_UNAUTHORIZED() public {
-    }
+    function testRevert_applyToPool_new_application_UNAUTHORIZED() public {}
 
-    function testRevert_applyToPool_update_application_UNAUTHORIZED() public {
-    }
+    function testRevert_applyToPool_update_application_UNAUTHORIZED() public {}
 
-    function test_getApplicationStatus() public {
-    }
+    function test_getApplicationStatus() public {}
 
-    function test_allocate_native() public {
-    }
+    function test_allocate_native() public {}
 
-    function test_allocate_erc20() public {
-    }
+    function test_allocate_erc20() public {}
 
-    function testRevert_allocate_NOT_ALLO() public {
-    }
+    function testRevert_allocate_NOT_ALLO() public {}
 
-    function testRevert_allocate_VOTING_NOT_OPEN() public {
-    }
+    function testRevert_allocate_VOTING_NOT_OPEN() public {}
 
-    function testRevert_allocate_ALLOCATION_AMOUNT_MISMATCH() public {
-    }
+    function testRevert_allocate_ALLOCATION_AMOUNT_MISMATCH() public {}
 
-    function testRevert_allocate_TRANSFER_FAILED() public {
-    }
+    function testRevert_allocate_TRANSFER_FAILED() public {}
 
+    function test_getPayout() public {}
 
-    function test_getPayout() public {
+    function test_reviewApplications() public {}
 
-    }
+    function testRevert_reviewApplications_UNAUTHORIZED() public {}
 
-    function test_reviewApplications() public {
-    }
+    function test_setPayout() public {}
 
-    function testRevert_reviewApplications_UNAUTHORIZED() public {
-    }
+    function test_setPayout_UNAUTHORIZED() public {}
 
-    function test_setPayout() public {
+    function test_setPayout_INVALID_INPUT() public {}
 
-    }
+    function test_setPayout_INVALID_APPLICATION() public {}
 
-    function test_setPayout_UNAUTHORIZED() public {
+    function test_claim() public {}
 
-    }
-
-    function test_setPayout_INVALID_INPUT() public {
-
-    }
-
-    function test_setPayout_INVALID_APPLICATION() public {
-
-    }
-
-    function test_claim() public {
-
-    }
-
-    function testRevert_claim_VOTING_NOT_ENDED() public {
-
-    }
+    function testRevert_claim_VOTING_NOT_ENDED() public {}
 
     function test_updateTimestamps() public {
         // Todo: test update timestamps
@@ -126,6 +99,5 @@ contract AllocationWithVotingBaseTest is Test {
     }
 
     /// @notice votingStartTime must be in the future
-    function testRevert_updateTimestamps_VSINFUTURE_INVALID_TIME() public {
-    }
+    function testRevert_updateTimestamps_VSINFUTURE_INVALID_TIME() public {}
 }
