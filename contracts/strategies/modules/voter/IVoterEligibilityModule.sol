fcf479df90d72165e8dcafc9a30bf70d4f1a6e0f
Zach Obront <zobront@gmail.com>
2023-07-12 11:04:07 -0500

    notes in all interfaces

--- contracts/strategies/modules/voter/IVoterEligibilityModule.sol
+++ contracts/strategies/modules/voter/IVoterEligibilityModule.sol
@@ -5,5 +5,9 @@ import { IStrategy }  from "../../IStrategy.sol";
 
 interface IVoterEligibilityModule {
     function initializeVoterEligibilityModule(bytes _data) external;
+    // used to initialize voter eligibility, for example with token address if token gated
+
     function isValidVoter(address _voter) public view returns (bool);
+    // simply returns whether a voter is valid or not
+    // this might need additional functions if, for example, it's based on allow list controlled by owner
 }
