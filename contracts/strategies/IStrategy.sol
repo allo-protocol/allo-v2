d1e15628ee4e86c7ad6e08d9567dda52c95fddd4
Zach Obront <zobront@gmail.com>
2023-07-12 10:56:43 -0500

    all interfaces into IStrategy.sol

--- contracts/strategies/IStrategy.sol
+++ contracts/strategies/IStrategy.sol
@@ -12,11 +12,21 @@ interface IStrategy {
         uint amount;
     }
 
-    // add interfaces for all the required functions in the different modules
-
     function initializeRecipientEligibiilityModule(bytes _data) external;
+    function registerRecipients(bytes memory _data, address _sender) external payable returns (address);
+    function getRecipientStatus(address _recipientId) public view returns (RecipientStatus);
+    function allRecipients() public view returns (address[] memory);
+
     function initializeVoterEligibilityModule(bytes _data) external;
+    function isValidVoter(address _voter) public view returns (bool);
+
     function initializeVotingModule(bytes _data) external;
+    function allocate(bytes memory _data, address _sender) external payable;
+    function getResults(address[] memory _recipientId, bytes memory _data) external view returns (ResultSummary[] memory);
+
     function initializeAllocationModule(bytes _data) external;
+    function getPayouts(address[] memory recipientIds, bytes memory _data) public view returns (PayoutSummary[])
+
     function initializeDistributionModule(bytes _data) external;
+    function distribute(address[] memory _recipientIds, bytes memory _data, address _sender) external;
 }
