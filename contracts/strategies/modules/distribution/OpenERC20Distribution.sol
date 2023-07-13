1bd9f1377be5b2da7f3e630fbaf1efe68be83023
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-07-13 11:28:14 +0530

    fmt

--- contracts/strategies/modules/distribution/OpenERC20Distribution.sol
+++ contracts/strategies/modules/distribution/OpenERC20Distribution.sol
@@ -7,7 +7,7 @@ import {ERC20} from "@openzeppelin/token/ERC20/ERC20.sol";
 
 contract OpenERC20Distribution is IDistributionModule, IStrategy {
     ERC20 token;
-    mapping (address => uint) public claimed;
+    mapping(address => uint256) public claimed;
 
     function initializeDistributionModule(bytes memory _data) external {
         token = ERC20(abi.decode(_data, (address)));
@@ -15,9 +15,9 @@ contract OpenERC20Distribution is IDistributionModule, IStrategy {
 
     function distribute(address[] memory _recipientIds, bytes memory _data, address _sender) external {
         PayoutSummary[] memory payouts = getPayouts(_recipientIds, _data);
-        for (uint i = 0; i < payouts.length; i++) {
+        for (uint256 i = 0; i < payouts.length; i++) {
             address recipient = payouts[i].recipientId;
-            uint amountOwed = payouts[i].amount - claimed[recipient];
+            uint256 amountOwed = payouts[i].amount - claimed[recipient];
             claimed[recipient] = payouts[i].amount;
             token.transfer(recipient, amountOwed);
         }
