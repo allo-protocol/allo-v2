465d935104e2c1832c8db28672d0173f8c3d690d
Zach Obront <zobront@gmail.com>
2023-07-14 09:29:48 -0500

    k

--- contracts/strategies/BaseStrategy.sol
+++ contracts/strategies/BaseStrategy.sol
@@ -70,7 +70,7 @@ abstract contract BaseStrategy is IStrategy, Transfer {
     /// @param _poolId Id of the pool
     /// @param _data The data to be decoded
     /// @dev This function is called by Allo.sol
-    function initialize(uint256 _poolId, bytes memory _data) external virtual onlyAllo {
+    function initialize(uint256 _poolId, bytes memory _data) public virtual onlyAllo {
         if (_poolId == 0) {
             revert BaseStrategy_INVALID_ADDRESS();
         }
