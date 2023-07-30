e91cb2aa20d79393077bcf9a700763117368ecd1
Zakk <zakk@hawthorne.io>
2023-07-30 10:15:09 -0500

    draft up wrapped voting spec

---
+++ contracts/strategies/wrapped-voting/WrappedVoting.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.19;
+
+// Interfaces
+import {IAllo} from "../../core/IAllo.sol";
+import {IRegistry} from "../../core/IRegistry.sol";
+// Core Contracts
+import {BaseStrategy} from "../BaseStrategy.sol";
+// Internal Libraries
+import {Metadata} from "../../core/libraries/Metadata.sol";
+
+contract RFPSimpleStrategy is BaseStrategy {
+
+}
