0b8a5200f06f1d6f16f03df6ecca084aa9b7f6a1
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-07-31 19:07:25 +0530

    wip deployer

---
+++ scripts/config/strategies.config.ts
@@ -0,0 +1,98 @@
+// NOTE: Update this file anytime a new strategy is deployed.
+
+type StrategyConfig = {
+  [key: string]: {
+    address: string;
+    name: string;
+    version: string;
+  };
+};
+
+type DeployParams = Record<number, StrategyConfig>;
+
+export const deployerAddress = {
+    // Mainnet
+    1: "",
+    // Goerli
+    5: "0x339d087B3ab5DF79c6c3617B2487e5340F2DBE3f",
+    // Sepolia
+    11155111: "0x339d087B3ab5DF79c6c3617B2487e5340F2DBE3f",
+    // PGN
+    424: "",
+    // PGN Sepolia
+    58008: "0x339d087B3ab5DF79c6c3617B2487e5340F2DBE3f",
+    // Optimism
+    10: "",
+    // Optimism Goerli
+    420: "0x339d087B3ab5DF79c6c3617B2487e5340F2DBE3f",
+    // Fantom
+    250: "",
+}
+
+// NOTE: This will be the version address for each registy on each network.
+export const nameConfig: DeployParams = {
+  // Mainnet
+  1: {
+    "direct-grants": {
+      name: "",
+      version: "",
+      address: "",
+    },
+  },
+  // Goerli
+  5: {
+    "direct-grants": {
+      name: "",
+      version: "",
+      address: "",
+    },
+  },
+  // Sepolia
+  11155111: {
+    "direct-grants": {
+      name: "",
+      version: "",
+      address: "",
+    },
+  },
+  // PGN
+  424: {
+    "direct-grants": {
+      name: "",
+      version: "",
+      address: "",
+    },
+  },
+  // PGN Sepolia
+  58008: {
+    "direct-grants": {
+      name: "",
+      version: "",
+      address: "",
+    },
+  },
+  // Optimism
+  10: {
+    "direct-grants": {
+      name: "",
+      version: "",
+      address: "",
+    },
+  },
+  // Optimism Goerli
+  420: {
+    "direct-grants": {
+      name: "",
+      version: "",
+      address: "",
+    },
+  },
+  // Fantom
+  250: {
+    "direct-grants": {
+      name: "",
+      version: "",
+      address: "",
+    },
+  },
+};
---
+++ scripts/core/deployStrategies.ts
@@ -0,0 +1,48 @@
+import hre, { ethers } from "hardhat";
+import { confirmContinue, prettyNum } from "../utils/script-utils";
+
+export async function deployStrategies() {
+  const network = await ethers.provider.getNetwork();
+  const networkName = await hre.network.name;
+  const chainId = Number(network.chainId);
+  const account = (await ethers.getSigners())[0];
+  const deployerAddress = await account.getAddress();
+  const blocksToWait = networkName === "localhost" ? 0 : 5;
+  const balance = await ethers.provider.getBalance(deployerAddress);
+
+  console.log(`
+    ////////////////////////////////////////////////////
+            Deploys Strategies.sol on ${networkName}
+    ////////////////////////////////////////////////////`
+  );
+
+  await confirmContinue({
+    contract: "Deploy Deployer.sol",
+    chainId: chainId,
+    network: networkName,
+    deployerAddress: deployerAddress,
+    balance: prettyNum(balance.toString()),
+  });
+
+  console.log("Deploying Deployer.sol...");
+
+  // get deployer address
+  const Deployer = await ethers.getContractFactory("Deployer");
+  // deployer.deploy()
+  
+//   for loop
+
+  // await instance.deploymentTransaction()?.wait(blocksToWait);
+
+  console.log("Deployer deployed to:", instance.target);
+
+  return instance.target;
+}
+
+// deployStrategies().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+
+// Note: Deploy script to run in terminal:
+// npx hardhat run scripts/deployStrategies.ts --network sepolia
