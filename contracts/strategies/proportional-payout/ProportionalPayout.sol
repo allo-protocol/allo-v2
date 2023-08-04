b30ba39b279a008f5ca59da865c90d9ab4f2d73c
KurtMerbeth <Kurt@Merbeth.io>
2023-08-04 15:07:51 +0200

    added Proportional Payout strategy

--- contracts/strategies/proportional-payout/ProportionalPayout.sol
+++ contracts/strategies/proportional-payout/ProportionalPayout.sol
@@ -1,193 +1,240 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import {ERC20} from "@solady/tokens/ERC20.sol";
-import {Allo} from "../../core/Allo.sol";
+import {ERC721} from "solady/src/tokens/ERC721.sol";
 import {IAllo} from "../../core/IAllo.sol";
 import {BaseStrategy} from "./../BaseStrategy.sol";
-import {Transfer} from "../../core/libraries/Transfer.sol";
+import {Metadata} from "../../core/libraries/Metadata.sol";
 
 contract ProportionalPayout is BaseStrategy {
-    error RECIPIENT_MAX_REACHED();
+    /// =====================
+    /// ======= Events ======
+    /// =====================
 
-    /// @notice The maximum number of recipients allowed
-    /// @dev This is both to keep the number of choices low and to avoid gas issues
-    uint256 constant MAX_RECIPIENTS = 3;
+    event AllocationTimeSet(uint256 startTime, uint256 endTime);
 
-    /// ================================
-    /// ========== Storage =============
-    /// ================================
+    /// =====================
+    /// ======= Errors ======
+    /// =====================
 
-    /// @notice When the allocation (voting) period starts
-    uint256 public startTime;
+    error RECIPIENT_ERROR(address recipientId);
+    error UNAUTHORIZED();
+    error MAX_REACHED();
+    error ALLOCATION_NOT_ACTIVE();
+    error ALLOCATION_NOT_ENDED();
+    error INVALID();
 
-    /// @notice When the allocation (voting) period ends
-    uint256 public endTime;
+    /// ======================
+    /// ====== Modifier ======
+    /// ======================
 
-    /// @notice The token required for voting
-    ERC20 public token;
-
-    /// @notice List of recipients who will receive payout at the end
-    address[] public recipients;
-
-    /// @notice Whether or not a recipient is valid
-    mapping(address => bool) public isRecipient;
-
-    /// @notice Votes for each recipient
-    mapping(address => uint256) public votes;
-
-    /// @notice Whether or not a voter has voted
-    /// @dev This is to prevent double voting
-    mapping(address => bool) public hasVoted;
-
-    /// @notice Total number of votes cast
-    /// @dev This is used to calculate the percentage of votes for each recipient at the end
-    uint256 public totalVotes;
-
-    /// ===============================
-    /// ========== Errors =============
-    /// ===============================
-
-    error BadTimes();
-    error NotWithinAllocationPeriod();
-    error AlreadyAllocated();
-    error NotElligibleVoter();
-    error AllocationHasntEnded();
-    error TooManyRecipients();
-
-    /// ===============================
-    /// ======== Constructor ==========
-    /// ===============================
-
-    constructor(address _allo, string memory _name) BaseStrategy(_allo, _name) {}
-
-    /// ===============================
-    /// ========= Modifiers ===========
-    /// ===============================
-
-    modifier onlyDuringAllocationPeriod() {
-        if (block.timestamp < startTime || block.timestamp > endTime) {
-            revert NotWithinAllocationPeriod();
+    modifier onlyActiveAllocation() {
+        if (allocationStartTime > block.timestamp || block.timestamp > allocationEndTime) {
+            revert ALLOCATION_NOT_ACTIVE();
         }
         _;
     }
 
-    /// ===============================
-    /// ========= Functions ===========
-    /// ===============================
-
-    function initialize(uint256 _poolId, bytes memory _data) public override {
-        __ProportionalPayout_init(_poolId, _data);
+    modifier onlyAfterAllocation() {
+        if (block.timestamp < allocationEndTime) {
+            revert ALLOCATION_NOT_ENDED();
+        }
+        _;
     }
 
-    function __ProportionalPayout_init(uint256 _poolId, bytes memory _data) internal {
+    /// ======================
+    /// ======= Storage ======
+    /// ======================
+    struct Recipient {
+        address recipientAddress;
+        Metadata metadata;
+        RecipientStatus recipientStatus;
+        uint256 totalVotesReceived;
+    }
+
+    /// @notice recipientId => Recipient
+    mapping(address => Recipient) public recipients;
+    /// @notice nftId => has allocated
+    mapping(uint256 => bool) public hasAllocated;
+    /// @notice recipientId => paid out
+    mapping(address => bool) public paidOut;
+
+    ERC721 public nft;
+
+    uint256 public maxRecipientsAllowed;
+    uint256 public recipientsCounter;
+    uint256 public allocationStartTime;
+    uint256 public allocationEndTime;
+    uint256 public totalAllocations;
+
+    // ===================
+    // === Constructor ===
+    // ===================
+
+    constructor(address _allo, string memory _name) BaseStrategy(_allo, _name) {}
+
+    // ==================
+    // ==== External ====
+    // ==================
+
+    function initialize(uint256 _poolId, bytes memory _data) external override onlyAllo {
+        (address _nft, uint256 _maxRecipientsAllowed, uint256 _allocationStartTime, uint256 _allocationEndTime) =
+            abi.decode(_data, (address, uint256, uint256, uint256));
         __BaseStrategy_init(_poolId);
 
-        (token, startTime, endTime) = abi.decode(_data, (ERC20, uint256, uint256));
+        nft = ERC721(_nft);
+        maxRecipientsAllowed = _maxRecipientsAllowed;
 
-        if (startTime >= endTime || endTime < block.timestamp) {
-            revert BadTimes();
-        }
+        _setAllocationTime(_allocationStartTime, _allocationEndTime);
     }
 
-    function _registerRecipient(bytes memory _data, address _sender)
+    /// @notice Get recipient status
+    /// @param _recipientId Id of the recipient
+    function getRecipientStatus(address _recipientId) external view returns (RecipientStatus) {
+        return recipients[_recipientId].recipientStatus;
+    }
+
+    /// @notice Checks if the allocator is valid
+    /// @param _allocator The allocator address
+    /// @return true if the allocator is valid
+    function isValidAllocator(address _allocator) external view virtual override returns (bool) {
+        return nft.balanceOf(_allocator) > 0;
+    }
+
+    /// @notice Set the allocation start and end timestamps
+    /// @param _allocationStartTime The allocation start timestamp
+    /// @param _allocationEndTime The allocation end timestamp
+    function setAllocationTime(uint256 _allocationStartTime, uint256 _allocationEndTime)
+        external
+        onlyPoolManager(msg.sender)
+    {
+        _setAllocationTime(_allocationStartTime, _allocationEndTime);
+    }
+
+    // ==================
+    // ==== Internal ====
+    // ==================
+    /// @notice Allocate votes to a recipient
+    /// @param _data The data
+    /// @param _sender The sender of the transaction
+    /// @dev Only the NFT holder can call this function
+
+    function _allocate(bytes memory _data, address _sender) internal override onlyActiveAllocation {
+        (address recipientId, uint256 nftId) = abi.decode(_data, (address, uint256));
+
+        if (hasAllocated[nftId] || nft.ownerOf(nftId) != _sender) {
+            revert UNAUTHORIZED();
+        }
+
+        hasAllocated[nftId] = true;
+
+        Recipient storage recipient = recipients[recipientId];
+
+        if (recipient.recipientStatus != RecipientStatus.Accepted) {
+            revert RECIPIENT_ERROR(recipientId);
+        }
+
+        recipient.totalVotesReceived++;
+        totalAllocations++;
+
+        emit Allocated(recipientId, 1, address(0), _sender);
+    }
+
+    /// @notice Distribute the tokens to the recipients
+    /// @param _recipientIds The recipient ids
+    /// @param _sender The sender of the transaction
+    function _distribute(address[] memory _recipientIds, bytes memory, address _sender)
         internal
         override
-        onlyDuringAllocationPeriod
-        onlyAllo
-        returns (address)
+        onlyAfterAllocation
     {
-        if (recipients.length == MAX_RECIPIENTS) {
-            revert RECIPIENT_MAX_REACHED();
-        }
+        uint256 payoutLength = _recipientIds.length;
+        for (uint256 i = 0; i < payoutLength;) {
+            address recipientId = _recipientIds[i];
+            Recipient storage recipient = recipients[recipientId];
 
-        address[] memory _recipients = abi.decode(_data, (address[]));
+            PayoutSummary memory payout = _getPayout(recipientId, "");
+            uint256 amount = payout.amount;
 
-        if (_recipients.length > MAX_RECIPIENTS) {
-            revert TooManyRecipients();
-        }
+            if (paidOut[recipientId] || recipient.recipientStatus != RecipientStatus.Accepted || amount == 0) {
+                revert RECIPIENT_ERROR(recipientId);
+            }
 
-        for (uint256 i; i < _recipients.length; i++) {
-            isRecipient[_recipients[i]] = true;
-            recipients.push(_recipients[i]);
+            IAllo.Pool memory pool = allo.getPool(poolId);
+            _transferAmount(pool.token, recipient.recipientAddress, amount);
+
+            paidOut[recipientId] = true;
+
+            emit Distributed(recipientId, recipient.recipientAddress, amount, _sender);
             unchecked {
-                ++i;
+                i++;
             }
         }
     }
 
-    function getRecipientStatus(address _recipientId) public view returns (RecipientStatus) {
-        if (isRecipient[_recipientId]) {
-            return RecipientStatus.Accepted;
-        } else {
-            return RecipientStatus.None;
-        }
+    /// @notice Get the payout for a single recipient
+    /// @param _recipientId The recipient id
+    /// @return The payout as a PayoutSummary struct
+    function _getPayout(address _recipientId, bytes memory) internal view override returns (PayoutSummary memory) {
+        Recipient storage recipient = recipients[_recipientId];
+
+        uint256 totalVotesReceived = recipient.totalVotesReceived;
+        uint256 amount = totalAllocations > 0 ? poolAmount * totalVotesReceived / totalAllocations : 0;
+
+        return PayoutSummary(recipient.recipientAddress, amount);
     }
 
-    function isValidAllocator(address _allocator) public view returns (bool) {
-        return token.balanceOf(_allocator) > 0;
-    }
-
-    function _allocate(bytes memory _data, address _sender) internal override onlyDuringAllocationPeriod onlyAllo {
-        if (hasVoted[_sender]) {
-            revert AlreadyAllocated();
-        }
-
-        (address _recipient, uint256 _amount) = abi.decode(_data, (address, uint256));
-
-        if (!isRecipient[_recipient]) {
-            revert NotElligibleVoter();
-        }
-
-        hasVoted[_sender] = true;
-        votes[_recipient] += _amount;
-        totalVotes += _amount;
-    }
-
-    function getPayouts(address[] memory _recipientIds, bytes memory _data, address _sender)
-        public
-        view
-        returns (PayoutSummary[] memory)
-    {
-        if (block.timestamp < endTime) {
-            revert AllocationHasntEnded();
-        }
-
-        uint256[] memory _payouts = new uint[](_recipientIds.length);
-        PayoutSummary[] memory payoutSummary = new PayoutSummary[](_recipientIds.length);
-
-        // Get pool size
-        // (,,, uint256 _poolSize,,,) = pools(poolId);
-
-        // // loop through recipients
-        // for (uint256 i; i < _recipientIds.length; i++) {
-        //     if (isRecipient[_recipientIds[i]]) {
-        //         // get votes for recipient as percentage of total votes
-        //         _payouts[i] = votes[_recipientIds[i]] * _poolSize / totalVotes;
-        //         payoutSummary[i] = PayoutSummary(_recipientIds[i], _payouts[i]);
-        //     }
-        // }
-
-        return payoutSummary;
-    }
-
-    function _distribute(address[] memory _recipientIds, bytes memory _data, address _sender)
+    /// @notice Submit application to pool
+    /// @param _data The data to be decoded: address recipientId, address recipientAddress, RecipientStatus status, Metadata memory metadata
+    /// @param _sender The sender of the transaction
+    function _registerRecipient(bytes memory _data, address _sender)
         internal
         override
-        onlyAllo
+        onlyPoolManager(_sender)
+        returns (address)
     {
-        // Check that round has ended
-        if (block.timestamp < endTime) {
-            revert AllocationHasntEnded();
+        (address recipientId, address recipientAddress, RecipientStatus status, Metadata memory metadata) =
+            abi.decode(_data, (address, address, RecipientStatus, Metadata));
+
+        if (recipientId == address(0) || recipientAddress == address(0)) {
+            revert RECIPIENT_ERROR(recipientId);
         }
 
-        // Get distribution
-        // (,, address tokenToDistribute, uint256 amountToDistribute,,,) = allo.pools(poolId);
-        // PayoutSummary[] memory _payouts = getPayouts(_recipientIds, _data, _sender);
+        Recipient storage recipient = recipients[recipientId];
 
-        // // Transfer pool to recipients
-        // for (uint256 i; i < _recipientIds.length; i++) {
-        //     _transferAmount(tokenToDistribute, _recipientIds[i], _payouts[i].amount);
-        // }
+        if (recipient.recipientStatus != RecipientStatus.Accepted && status == RecipientStatus.Accepted) {
+            recipientsCounter++;
+        } else if (recipient.recipientStatus == RecipientStatus.Accepted && status == RecipientStatus.Rejected) {
+            recipientsCounter--;
+        } else {
+            revert RECIPIENT_ERROR(recipientId);
+        }
+
+        if (recipientsCounter > maxRecipientsAllowed) {
+            revert MAX_REACHED();
+        }
+
+        // update the recipients data
+        recipient.recipientAddress = recipientAddress;
+        recipient.metadata = metadata;
+        recipient.recipientStatus = status;
+
+        emit Registered(recipientId, _data, _sender);
+
+        return recipientId;
+    }
+
+    /// @notice Set the allocation start and end timestamps
+    /// @param _allocationStartTime The allocation start timestamp
+    /// @param _allocationEndTime The allocation end timestamp
+    function _setAllocationTime(uint256 _allocationStartTime, uint256 _allocationEndTime) internal {
+        // time stamps must be in future and end time after start time.
+        if (_allocationStartTime < block.timestamp || _allocationEndTime < _allocationStartTime) {
+            revert INVALID();
+        }
+        allocationStartTime = _allocationStartTime;
+        allocationEndTime = _allocationEndTime;
+
+        emit AllocationTimeSet(_allocationStartTime, _allocationEndTime);
     }
 }
