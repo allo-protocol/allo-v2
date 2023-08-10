6bc19abd6994cd95ab0542c6debd4c9344f2a2b5
Jaxcoder <codenamejason@outlook.com>
2023-08-10 14:04:58 -0400

    test: 100% coverage

--- contracts/strategies/donation-voting-custom-registry/SimpleProjectRegistry.sol
+++ contracts/strategies/donation-voting-custom-registry/SimpleProjectRegistry.sol
@@ -20,20 +20,20 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 /// @title Simple Project Registry
 /// @author @0xZakk <zakk@gitcoin.co>
 /// @notice This contract is a simple implementation of a registry. It is
-//intended to show that strategies can leverage their own registries. It assumes
-//an owner, like a DAO, with explicit permission to add and remove projects from
-//the registry.
+/// intended to show that strategies can leverage their own registries. It assumes
+/// an owner, like a DAO, with explicit permission to add and remove projects from
+/// the registry.
 contract SimpleProjectRegistry is Ownable {
-    ///////////////////////////////
-    ////////// Variables //////////
-    ///////////////////////////////
+    //===========================
+    //====== Variables ==========
+    //===========================
 
     /// @notice The projects in the registry
     mapping(address => bool) public projects;
 
-    ////////////////////////////
-    ////////// Events //////////
-    ////////////////////////////
+    //===========================
+    //========= Events ==========
+    //===========================
 
     /// @notice Emitted when a project is added to the Registry
     /// @param project The project that was added
@@ -43,24 +43,25 @@ contract SimpleProjectRegistry is Ownable {
     /// @param project The project that was removed
     event ProjectRemoved(address indexed project);
 
-    ////////////////////////////
-    ////////// Errors //////////
-    ////////////////////////////
+    //===========================
+    //========= Errors =========
+    //===========================
 
     /// @notice Error when a project is already in the registry
-    error ProjectAlreadyInRegistry();
+    error ALREADY_EXISTS();
+    error DOESNT_EXIST();
 
-    ////////////////////////////
-    /////// Constructor ////////
-    ////////////////////////////
+    //===========================
+    //===== Constructor =========
+    //===========================
 
     constructor(address _initialOwner) {
         _transferOwnership(_initialOwner);
     }
 
-    ////////////////////////////
-    ///////// Methods //////////
-    ////////////////////////////
+    //===========================
+    //========= Methods =========
+    //===========================
 
     /// @notice Add a project to the registry
     /// @param _project The project to add
@@ -70,7 +71,7 @@ contract SimpleProjectRegistry is Ownable {
 
     /// @notice Add an array projects to the registry
     /// @param _projects The projects to add
-    function addProject(address[] calldata _projects) external onlyOwner {
+    function addProjects(address[] calldata _projects) external onlyOwner {
         for (uint256 i = 0; i < _projects.length; i++) {
             _addProject(_projects[i]);
         }
@@ -90,22 +91,29 @@ contract SimpleProjectRegistry is Ownable {
         }
     }
 
-    ////////////////////////////
-    ///// Internal Methods /////
-    ////////////////////////////
+    //===========================
+    //==== Internal Methods =====
+    //===========================
 
     /// @notice Internal method that adds projects to the registry
     /// @param _project The address of the project to add
     function _addProject(address _project) internal {
         if (projects[_project]) {
-            revert ProjectAlreadyInRegistry();
+            revert ALREADY_EXISTS();
         }
         projects[_project] = true;
+
+        emit ProjectAdded(_project);
     }
 
     /// @notice Internal method that removes a project from the registry
     /// @param _project The address of the project to remove
     function _removeProject(address _project) internal {
+        if (!projects[_project]) {
+            revert DOESNT_EXIST();
+        }
         projects[_project] = false;
+
+        emit ProjectRemoved(_project);
     }
 }
