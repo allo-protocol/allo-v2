7b1274cd03ed61464c8858a93c2f96c03d519f81
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-08-17 16:49:39 +0530

    Test script (#222)
    * chore: update profile script
    
    * chore: update script
    
    * f'

--- scripts/test/createPool.ts
+++ scripts/test/createPool.ts
@@ -1,6 +1,7 @@
 import hre, { ethers } from "hardhat";
 import { alloConfig } from "../config/allo.config";
 import { confirmContinue, prettyNum } from "../utils/scripts";
+import {AbiCoder} from "ethers";
 
 export async function createPool() {
 
@@ -28,18 +29,50 @@ export async function createPool() {
     console.log("Creating pool...");
     const instance = await ethers.getContractAt('Allo', allo);
 
-    await instance.createPoolWithCustomStrategy({
-        // profileId: ,
-        // strategyId: ,
-        // initStrategyData: ,
-        // token: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
-        // amount: 0,
-        // metadata: {
-        //     protocol: 1,
-        //     pointer: "bafybeia4khbew3r2mkflyn7nzlvfzcb3qpfeftz5ivpzfwn77ollj47gqi"
-        // },
-        // managers: []
-    });
+    const _currentTimestamp = (await ethers.provider.getBlock(
+        await ethers.provider.getBlockNumber())
+    )!.timestamp;
+
+    const encoder = new AbiCoder();
+    const initStrategyData = encoder.encode(
+        ["bool", "bool", "uint256", "uint256", "uint256", "uint256", "address[]"],
+        [
+            true, // useRegistryAnchor
+            true, // metadataRequired
+            _currentTimestamp + 3600,   // 1 hour later   registrationStartTime
+            _currentTimestamp + 432000, // 5 days later   registrationEndTime
+            _currentTimestamp + 7200,   // 2 hours later  allocationStartTime
+            _currentTimestamp + 864000, // 10 days later  allocaitonEndTime
+            ['0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'] // allowed token
+        ]
+    );
+
+
+    console.log(        '0xb2a02f1bb0d07456d929eb7bf31c49b69f756ba90f64c0288488fb1a4cd6abef', // _profileId
+    '0xC88612a4541A28c221F3d03b6Cf326dCFC557C4E', // _strategy donationVotingMerkleStrategy
+    initStrategyData,   // _initStrategyData
+    '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', // _token
+    0, // _amount
+    {
+        protocol: 1,
+        pointer: "bafybeia4khbew3r2mkflyn7nzlvfzcb3qpfeftz5ivpzfwn77ollj47gqi"
+    }, // _metadata
+    ['0xB8cEF765721A6da910f14Be93e7684e9a3714123'] // _managers
+    );
+    
+
+    await instance.createPool(
+        '0xb2a02f1bb0d07456d929eb7bf31c49b69f756ba90f64c0288488fb1a4cd6abef', // _profileId
+        '0xC88612a4541A28c221F3d03b6Cf326dCFC557C4E', // _strategy donationVotingMerkleStrategy
+        initStrategyData,   // _initStrategyData
+        '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', // _token
+        0, // _amount
+        {
+            protocol: 1,
+            pointer: "bafybeia4khbew3r2mkflyn7nzlvfzcb3qpfeftz5ivpzfwn77ollj47gqi"
+        }, // _metadata
+        ['0xB8cEF765721A6da910f14Be93e7684e9a3714123'] // _managers
+    );
 
     console.log("pool created at:", instance.target);
   
