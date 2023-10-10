1a42d43cbb42d0b5bc68339891f5dce7e80e3797
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-10-10 12:58:35 +0530

    fix: update _checkOnlyAfterAllocation logic (#337)
    refs: https://github.com/sherlock-audit/2023-09-Gitcoin-judging/issues/839

--- contracts/strategies/_poc/proportional-payout/ProportionalPayoutStrategy.sol
+++ contracts/strategies/_poc/proportional-payout/ProportionalPayoutStrategy.sol
@@ -159,7 +159,7 @@ contract ProportionalPayoutStrategy is BaseStrategy {
     /// @notice Checks if the allocation has ended
     /// @dev Reverts if the allocation has not ended
     function _checkOnlyAfterAllocation() internal view {
-        if (block.timestamp < allocationEndTime) {
+        if (block.timestamp <= allocationEndTime) {
             revert ALLOCATION_NOT_ENDED();
         }
     }
