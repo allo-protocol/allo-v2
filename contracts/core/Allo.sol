71572c2adef8df8b5091219b1062fac332203151
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-10-16 16:13:11 +0530

    fix: make sure to use upgradeable access control (#374)
    refs https://github.com/sherlock-audit/2023-09-Gitcoin-judging/issues/852

--- contracts/core/Allo.sol
+++ contracts/core/Allo.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.19;
 import "solady/src/auth/Ownable.sol";
 import "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 import "openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
-import "openzeppelin-contracts/contracts/access/AccessControl.sol";
+import "openzeppelin-contracts-upgradeable/contracts/access/AccessControlUpgradeable.sol";
 import "openzeppelin-contracts-upgradeable/contracts/security/ReentrancyGuardUpgradeable.sol";
 // Interfaces
 import "./interfaces/IAllo.sol";
@@ -35,7 +35,16 @@ import {Transfer} from "./libraries/Transfer.sol";
 /// @author @thelostone-mc <aditya@gitcoin.co>, @0xKurt <kurt@gitcoin.co>, @codenamejason <jason@gitcoin.co>, @0xZakk <zakk@gitcoin.co>, @nfrgosselin <nate@gitcoin.co>
 /// @notice This contract is used to create & manage pools as well as manage the protocol.
 /// @dev The contract must be initialized with the 'initialize()' function.
-contract Allo is IAllo, Native, Transfer, Initializable, Ownable, AccessControl, ReentrancyGuardUpgradeable, Errors {
+contract Allo is
+    IAllo,
+    Native,
+    Transfer,
+    Initializable,
+    Ownable,
+    AccessControlUpgradeable,
+    ReentrancyGuardUpgradeable,
+    Errors
+{
     // ==========================
     // === Storage Variables ====
     // ==========================
