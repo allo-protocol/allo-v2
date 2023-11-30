5eeb0fca2c5ff866b395b241a56c92021638eddd
Kurt <kurt@merbeth.io>
2023-11-30 07:32:45 +0100

    removed try catch (#431)
    * removed try catch
    
    @codenamejason <3 great idea
    
    * fmt

--- contracts/strategies/_poc/micro-grants/MicroGrantsGovStrategy.sol
+++ contracts/strategies/_poc/micro-grants/MicroGrantsGovStrategy.sol
@@ -66,15 +66,20 @@ contract MicroGrantsGovStrategy is MicroGrantsBaseStrategy {
         = abi.decode(_data, (InitializeParams, UniversalGov, uint256, uint256));
         __MicroGrants_init(_poolId, initializeParams);
 
-        // sanity check if gov token is a supported token
-        try _gov.getPriorVotes(address(1234), 0) returns (uint96) {
+        bytes memory prior =
+            abi.encodeWithSelector(_gov.getPriorVotes.selector, abi.encodePacked(address(123), uint256(0)));
+        bytes memory past =
+            abi.encodeWithSelector(_gov.getPastVotes.selector, abi.encodePacked(address(123), uint256(0)));
+
+        (bool successPrior,) = address(_gov).call(prior);
+        (bool successPast,) = address(_gov).call(past);
+
+        if (successPrior) {
             govType = GovType.PriorVotes;
-        } catch {
-            try _gov.getPastVotes(address(1234), 0) returns (uint256) {
-                govType = GovType.PastVotes;
-            } catch {
-                revert INVALID_ADDRESS();
-            }
+        } else if (successPast) {
+            govType = GovType.PastVotes;
+        } else {
+            revert INVALID_ADDRESS();
         }
 
         gov = _gov;
