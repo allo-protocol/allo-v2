973bd775130fe049583fed266d6cd337bfca26a9
Jaxcoder <codenamejason@outlook.com>
2023-12-06 14:54:54 -0500

    add placeholders for coverage

---
+++ test/foundry/strategies/RecipientSuperApp.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.19;
+
+contract RecipientSuperAppTest {
+    error ZERO_ADDRESS();
+
+    function setUp() public {}
+
+    function test_deploy() public {}
+
+    function testRevert_deploy_ZERO_ADDRESS() public {}
+
+    function test_onFlowUpdated() public {}
+}
--- test/foundry/strategies/SQFSuperFluidStrategy.t.sol
+++ test/foundry/strategies/SQFSuperFluidStrategy.t.sol
@@ -1,8 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.19;
 
-import {SuperAppBaseSQFTest} from "./SuperAppBaseSQFTest.t.sol";
+import {SuperAppBaseSQF} from "./SuperAppBaseSQF.t.sol";
 
-contract SQFSuperFluidStrategyTest is SuperAppBaseSQFTest {
-// todo: add test cases
+contract SQFSuperFluidStrategyTest {
+    SuperAppBaseSQF _strategy;
+
+    function setUp() public {}
+
+    function test_getRecipient() public {}
+
+    function test_getRecipientStatus() public {}
+
+    function testRevert_registerRecipient_INVALID_METADATA() public {}
+
+    function testRevert_registerRecipient_UNAUTHORIZED() public {}
+
+    function testRevert_registerRecipient_UNAUTHORIZED_already_allocated() public {}
+
+    function testRevert_registerRecipient_RECIPIENT_ERROR_zero_recipientAddress() public {}
 }
