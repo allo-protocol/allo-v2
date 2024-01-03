315db8cffd3402cfcc176642e3b1c85fb78f2ebc
jordan <jordan.lesich@gmail.com>
2024-01-02 19:40:00 -0800

    pre-import

--- test/foundry/strategies/grant-ships/GrantShipStrategy.t.sol
+++ test/foundry/strategies/grant-ships/GrantShipStrategy.t.sol
@@ -3,18 +3,24 @@ pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
 
+// External Libraries
+// import {Hats} from "hats-protocol/Hats.sol";
+
 // Interfaces
-import {IStrategy} from "../../../contracts/core/interfaces/IStrategy.sol";
+import {IStrategy} from "../../../../contracts/core/interfaces/IStrategy.sol";
+
 // Strategy contracts
-import {GrantShipStrategy} from "../../../contracts/strategies/_poc/grant-ships/GrantShipStrategy.sol";
+import {GrantShipStrategy} from "../../../../contracts/strategies/_poc/grant-ships/GrantShipStrategy.sol";
+
 // Internal libraries
-import {Errors} from "../../../contracts/core/libraries/Errors.sol";
-import {Metadata} from "../../../contracts/core/libraries/Metadata.sol";
-import {Native} from "../../../contracts/core/libraries/Native.sol";
+import {Errors} from "../../../../contracts/core/libraries/Errors.sol";
+import {Metadata} from "../../../../contracts/core/libraries/Metadata.sol";
+import {Native} from "../../../../contracts/core/libraries/Native.sol";
+
 // Test libraries
-import {AlloSetup} from "../shared/AlloSetup.sol";
-import {RegistrySetupFull} from "../shared/RegistrySetup.sol";
-import {EventSetup} from "../shared/EventSetup.sol";
+import {AlloSetup} from "../../shared/AlloSetup.sol";
+import {RegistrySetupFull} from "../../shared/RegistrySetup.sol";
+import {EventSetup} from "../../shared/EventSetup.sol";
 
 contract GrantShiptStrategyTest is Test, RegistrySetupFull, AlloSetup, Native, EventSetup, Errors {
     Metadata public shipMetadata;
@@ -79,6 +85,51 @@ contract GrantShiptStrategyTest is Test, RegistrySetupFull, AlloSetup, Native, E
 
         strategyClone = payable(address(allo().getPool(newPoolId).strategy));
     }
+
+    // function _createHatsTree() internal {
+    //     hats = new Hats("Test Name", "ipfs://");
+
+    //     _topHatId = hats.mintTopHat(_topHatWearer, "testTopHat", "https://wwww/tophat.com/");
+
+    //     vm.prank(_topHatWearer);
+
+    //     _facilitatorHatId = hats.createHat(_topHatId, "Facilitator Hat", 2, _eligibility, _toggle, true, "");
+
+    //     vm.prank(_topHatWearer);
+    //     hats.mintHat(_facilitatorHatId, _gameFacilitator);
+
+    //     for (uint32 i = 0; i < 3;) {
+    //         vm.prank(_topHatWearer);
+    //         _shipHatIds[i] = hats.createHat(
+    //             _topHatId, string.concat("Ship Hat ", vm.toString(i + 1)), 1, _eligibility, _toggle, true, ""
+    //         );
+
+    //         _shipAddresses[i] = address(uint160(50 + i));
+
+    //         vm.prank(_topHatWearer);
+    //         hats.mintHat(_shipHatIds[i], _shipAddresses[i]);
+
+    //         vm.prank(_shipAddresses[i]);
+    //         _operatorHatIds[i] = hats.createHat(
+    //             _shipHatIds[i],
+    //             string.concat("Ship Operator Hat ", vm.toString(i + 1)),
+    //             3,
+    //             address(555),
+    //             address(333),
+    //             true,
+    //             ""
+    //         );
+
+    //         _shipOperators[i] = address(uint160(10 + i));
+
+    //         vm.prank(_topHatWearer);
+    //         hats.mintHat(_operatorHatIds[i], _shipOperators[i]);
+
+    //         unchecked {
+    //             ++i;
+    //         }
+    //     }
+    // }
 
     function test() public {}
 }
