18d203f4c2d32224f784c5d5747f7885b86421e8
jordan <jordan.lesich@gmail.com>
2024-01-04 14:18:09 -0800

    GameState Init rough

--- contracts/strategies/_poc/grant-ships/GameManager.sol
+++ contracts/strategies/_poc/grant-ships/GameManager.sol
@@ -2,15 +2,56 @@
 pragma solidity ^0.8.19;
 
 import {GrantShipStrategy} from "./GrantShipStrategy.sol";
+import {BaseStrategy} from "../../BaseStrategy.sol";
 
-contract GameManager {
+contract GameManager is BaseStrategy {
+    /// ================================
+    /// ========== Models ==============
+    /// ================================
+
+    /// ===============================
+    /// ======== Game State ===========
+    /// ===============================
+
     uint256 public currentRoundId;
     uint256 public currentRoundStartTime;
     uint256 public currentRoundEndTime;
+    address token;
 
-    GrantShipStrategy[] grantShips;
+    mapping(uint256 => GrantShipStrategy) public grantShips;
 
     uint256 gameFacilitatorHatId;
 
-    constructor() {}
+    /// ===============================
+    /// ======== Constructor ==========
+    /// ===============================
+
+    constructor(address _allo, string memory _name) BaseStrategy(_allo, _name) {}
+
+    /// ===============================
+    /// ======== Initialize ===========
+    /// ===============================
+
+    function initialize(uint256 _poolId, bytes memory _data) external {
+        __BaseStrategy_init(_poolId);
+
+        (bytes _gameParams, bytes[] _startingShips) = abi.decode(_data, (bytes, bytes[]));
+
+        __GameState_init(_gameParams);
+    }
+
+    function __GameState_init() internal {
+        // TODO: refactor if only 2 setup params
+        (uint256 _gameFacilitatorId, address _token) = abi.decode(_gameData, (uint256, address));
+
+        gameFacilitatorHatId = _gameFacilitatorId;
+        token = _token;
+
+        unchecked {
+            currentRoundId++;
+        }
+    }
+
+    /// @notice This contract should be able to receive native token
+    receive() external payable {}
 }
