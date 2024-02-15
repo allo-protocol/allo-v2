be7ecaca7d618da24537bef6ff2a7f3fb90c3e0e
Andrea Franz <andrea@gravityblast.com>
2024-02-15 15:46:00 +0100

    rename and format permit sig utils contracts

--- test/utils/PermitSigUtils.sol
+++ test/utils/PermitSigUtils.sol
@@ -1,49 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract SigUtilsDAI {
+contract PermitSigUtils {
     bytes32 internal DOMAIN_SEPARATOR;
 
     constructor(bytes32 _DOMAIN_SEPARATOR) {
         DOMAIN_SEPARATOR = _DOMAIN_SEPARATOR;
     }
 
-     // bytes32 public constant PERMIT_TYPEHASH = keccak256("Permit(address holder,address spender,uint256 nonce,uint256 expiry,bool allowed)");
-    bytes32 public constant PERMIT_TYPEHASH = 0xea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb;
+    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 public constant PERMIT_TYPEHASH =
+        0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
 
     struct Permit {
-        address holder;
+        address owner;
         address spender;
+        uint256 value;
         uint256 nonce;
-        uint256 expiry;
-        bool allowed;
+        uint256 deadline;
     }
 
     // computes the hash of a permit
-    function getStructHash(Permit memory _permit)
-        internal
-        pure
-        returns (bytes32)
-    {
+    function getStructHash(
+        Permit memory _permit
+    ) internal pure returns (bytes32) {
         return
             keccak256(
                 abi.encode(
                     PERMIT_TYPEHASH,
-                    _permit.holder,
+                    _permit.owner,
                     _permit.spender,
+                    _permit.value,
                     _permit.nonce,
-                    _permit.expiry,
-                    _permit.allowed
+                    _permit.deadline
                 )
             );
     }
 
     // computes the hash of the fully encoded EIP-712 message for the domain, which can be used to recover the signer
-    function getTypedDataHash(Permit memory _permit)
-        public
-        view
-        returns (bytes32)
-    {
+    function getTypedDataHash(
+        Permit memory _permit
+    ) public view returns (bytes32) {
         return
             keccak256(
                 abi.encodePacked(
