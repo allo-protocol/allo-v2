cfce8b86686e807da07c11c6e96ae8a116e980b3
0xKurt <Kurt@Merbeth.io>
2024-03-11 15:37:55 +0100

    audit: fix signature length

--- contracts/strategies/donation-voting-merkle-distribution-direct-transfer/DonationVotingMerkleDistributionDirectTransferStrategy.sol
+++ contracts/strategies/donation-voting-merkle-distribution-direct-transfer/DonationVotingMerkleDistributionDirectTransferStrategy.sol
@@ -107,27 +107,18 @@ contract DonationVotingMerkleDistributionDirectTransferStrategy is DonationVotin
     }
 
     function splitSignature(bytes memory sig) public pure returns (bytes32 r, bytes32 s, uint8 v) {
-        require(sig.length == 65, "invalid signature length");
-
-        assembly {
-            /*
-            First 32 bytes stores the length of the signature
-
-            add(sig, 32) = pointer of sig + 32
-            effectively, skips first 32 bytes of signature
-
-            mload(p) loads next 32 bytes starting at the memory address p into memory
-            */
-
-            // first 32 bytes, after the length prefix
-            r := mload(add(sig, 32))
-            // second 32 bytes
-            s := mload(add(sig, 64))
-            // final byte (first byte of the next 32 bytes)
-            v := byte(0, mload(add(sig, 96)))
+        if (sig.length == 65) {
+            (r, s) = abi.decode(sig, (bytes32, bytes32));
+            v = uint8(sig[64]);
+        } else if (sig.length == 64) {
+            // EIP-2098
+            bytes32 vs;
+            (r, vs) = abi.decode(sig, (bytes32, bytes32));
+            s = vs & (0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+            v = uint8(uint256(vs >> 255)) + 27;
+        } else {
+            revert INVALID();
         }
-
-        // implicitly return (r, s, v)
     }
 
     /// @notice Internal function to return the token amount locked in vault
