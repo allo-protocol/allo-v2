5ed26d3dfaf22152b4e6e974b8b3e59813604405
bitbeckers <code@bitbeckers.com>
2024-03-15 16:22:58 +0100

    feat(init): pseudo setup cross-chain bridging strategy

---
+++ contracts/strategies/cross-chain-donation-voting-merkle-distribution-direct-transfer/DonationVotingMerkleDistributionDirectTransferStrategy.sol
@@ -0,0 +1,180 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.19;
+
+import {ISignatureTransfer} from "permit2/ISignatureTransfer.sol";
+import {DonationVotingMerkleDistributionBaseStrategy} from
+    "../donation-voting-merkle-base/DonationVotingMerkleDistributionBaseStrategy.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
+import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IDAI} from "../donation-voting-merkle-base/IDAI.sol";
+
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⢿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⡟⠘⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣿⣿⣿⣾⠻⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⡿⠀⠀⠸⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⢀⣠⣴⣴⣶⣶⣶⣦⣦⣀⡀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⡿⠃⠀⠙⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⠁⠀⠀⠀⢻⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠘⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⠃⠀⠀⠀⠀⠈⢿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⣰⣿⣿⣿⡿⠋⠁⠀⠀⠈⠘⠹⣿⣿⣿⣿⣆⠀⠀⠀
+// ⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⢰⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⡀⠀⠀
+// ⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣟⠀⡀⢀⠀⡀⢀⠀⡀⢈⢿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⠀⠀
+// ⠀⠀⣠⣿⣿⣿⣿⣿⣿⡿⠋⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⡿⢿⠿⠿⠿⠿⠿⠿⠿⠿⠿⢿⣿⣿⣿⣷⡀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠸⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⠂⠀⠀
+// ⠀⠀⠙⠛⠿⠻⠻⠛⠉⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣧⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⢻⣿⣿⣿⣷⣀⢀⠀⠀⠀⡀⣰⣾⣿⣿⣿⠏⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣧⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠹⢿⣿⣿⣿⣿⣾⣾⣷⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠙⠋⠛⠙⠋⠛⠙⠋⠛⠙⠋⠃⠀⠀⠀⠀⠀⠀⠀⠀⠠⠿⠻⠟⠿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠟⠿⠟⠿⠆⠀⠸⠿⠿⠟⠯⠀⠀⠀⠸⠿⠿⠿⠏⠀⠀⠀⠀⠀⠈⠉⠻⠻⡿⣿⢿⡿⡿⠿⠛⠁⠀⠀⠀⠀⠀⠀
+//                    allo.gitcoin.co
+/// @title Donation Voting Merkle Distribution Strategy
+/// @author @thelostone-mc <aditya@gitcoin.co>, @0xKurt <kurt@gitcoin.co>, @codenamejason <jason@gitcoin.co>, @0xZakk <zakk@gitcoin.co>, @nfrgosselin <nate@gitcoin.co>, @bitbeckers
+/// @notice Strategy for donation voting allocation with a merkle distribution
+contract DonationVotingMerkleDistributionDirectTransferStrategy is DonationVotingMerkleDistributionBaseStrategy {
+    /// ===============================
+    /// ======== Constructor ==========
+    /// ===============================
+
+    /// @notice Constructor for the Donation Voting Merkle Distribution Strategy
+    /// @param _allo The 'Allo' contract
+    /// @param _name The name of the strategy
+    constructor(address _allo, string memory _name, ISignatureTransfer _permit2)
+        DonationVotingMerkleDistributionBaseStrategy(_allo, _name, _permit2)
+    {}
+
+    /// ================================
+    /// ============ Hooks =============
+    /// ================================
+
+    /// @notice After allocation hook to transfer the allocated tokens to the recipients
+    /// @param _data The encoded recipientId, amount and token
+    /// @param _sender The sender of the allocation
+    /// @dev Note that in the cross-chain context the tokens are held by the BridgeAdapter contract
+    function _afterAllocate(bytes memory _data, address _sender) internal override {
+        // Decode the '_data' to get the recipientId, amount and token
+        (address recipientId, PermitType permitType, Permit2Data memory p2Data) =
+            abi.decode(_data, (address, PermitType, Permit2Data));
+
+        // Get the token address
+        address token = p2Data.permit.permitted.token;
+        uint256 amount = p2Data.permit.permitted.amount;
+
+        if (token == NATIVE) {
+            if (msg.value < amount) {
+                revert AMOUNT_MISMATCH();
+            }
+            SafeTransferLib.safeTransferETH(_recipients[recipientId].recipientAddress, amount);
+        } else if (permitType == PermitType.Permit2) {
+            //TODO Do we want infinite approval between the BridgeAdapter and AlloV2? With low as cost we could wrap approve and transfer
+            SafeTransferLib.safeTransferFrom(token, _sender, _recipients[recipientId].recipientAddress, amount);
+            // TODO can we use the permit flow?
+    //            PERMIT2.permitTransferFrom( // The permit message.
+    //                p2Data.permit,
+    //                // The transfer recipient and amount.
+    //                ISignatureTransfer.SignatureTransferDetails({
+    //                    to: _recipients[recipientId].recipientAddress,
+    //                    requestedAmount: amount
+    //                }),
+    //                // Owner of the tokens and signer of the message.
+    //                _sender,
+    //                // The packed signature that was the result of signing
+    //                // the EIP712 hash of `_permit`.
+    //                p2Data.signature
+    //            );
+        } else if (permitType == PermitType.Permit) {
+            (bytes32 r, bytes32 s, uint8 v) = splitSignature(p2Data.signature);
+            // The tx can be front-run, and another user can use the permit message and signature to invalidate the nonce.
+            // In this case the permit call will fail, but it means that the contract already has allowance for the token.
+            try IERC20Permit(token).permit(_sender, address(this), amount, p2Data.permit.deadline, v, r, s) {}
+            catch Error(string memory reason) {
+                if (IERC20(token).allowance(msg.sender, address(this)) < amount) {
+                    revert(reason);
+                }
+            } catch (bytes memory reason) {
+                if (IERC20(token).allowance(msg.sender, address(this)) < amount) {
+                    revert(string(reason));
+                }
+            }
+            IERC20(token).transferFrom(_sender, _recipients[recipientId].recipientAddress, amount);
+        } else if (permitType == PermitType.PermitDAI) {
+            (bytes32 r, bytes32 s, uint8 v) = splitSignature(p2Data.signature);
+            // The tx can be front-run, and another user can use the permit message and signature to invalidate the nonce.
+            // In this case the permit call will fail, but it means that the contract already has allowance for the token.
+            try IDAI(token).permit(_sender, address(this), p2Data.permit.nonce, p2Data.permit.deadline, true, v, r, s) {}
+            catch Error(string memory reason) {
+                if (IERC20(token).allowance(msg.sender, address(this)) < amount) {
+                    revert(reason);
+                }
+            } catch (bytes memory reason) {
+                if (IERC20(token).allowance(msg.sender, address(this)) < amount) {
+                    revert(string(reason));
+                }
+            }
+            IERC20(token).transferFrom(_sender, _recipients[recipientId].recipientAddress, amount);
+        }
+    }
+
+    function splitSignature(bytes memory sig) public pure returns (bytes32 r, bytes32 s, uint8 v) {
+        require(sig.length == 65, "invalid signature length");
+
+        assembly {
+            /*
+            First 32 bytes stores the length of the signature
+
+            add(sig, 32) = pointer of sig + 32
+            effectively, skips first 32 bytes of signature
+
+            mload(p) loads next 32 bytes starting at the memory address p into memory
+            */
+
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        // implicitly return (r, s, v)
+    }
+
+    /// @notice Internal function to return the token amount locked in vault
+    /// @dev This function will return 0 since all funds are transferred directly to the recipient
+    function _tokenAmountInVault(address) internal pure override returns (uint256) {
+        return 0;
+    }
+
+    /// ================================
+    /// ========== Overrides ===========
+    /// ================================
+
+    /// @notice Allocate tokens to recipient.
+    /// @dev This can only be called during the allocation period. Emts an 'Allocated()' event.
+    /// @param _data The data to be decoded
+    /// @custom:data (address recipientId, uint256 amount, address token)
+    /// @param _sender The sender of the transaction
+    /// @dev In the context of this strategy, the _sender is the BridgeAdapter
+    function _allocate(bytes memory _data, address _sender) internal virtual override onlyActiveAllocation {
+        // Decode the '_data' to get the recipientId, amount and token
+        (address recipientId, address originSender,, Permit2Data memory p2Data) = abi.decode(_data, (address, address, PermitType, Permit2Data));
+
+        uint256 amount = p2Data.permit.permitted.amount;
+        address token = p2Data.permit.permitted.token;
+
+        //TODO validate sender (signer === originSender)
+
+        // If the recipient status is not 'Accepted' this will revert, the recipient must be accepted through registration
+        if (Status(_getUintRecipientStatus(recipientId)) != Status.Accepted) {
+            revert RECIPIENT_ERROR(recipientId);
+        }
+
+        // The token must be in the allowed token list and not be native token or zero address
+        if (!allowedTokens[token] && !allowedTokens[address(0)]) {
+            revert INVALID();
+        }
+
+        // If the token is native, the amount must be equal to the value sent, otherwise it reverts
+        if ((msg.value > 0 && token != NATIVE) || (token == NATIVE && msg.value != amount)) {
+            revert INVALID();
+        }
+
+        // Emit that the amount has been allocated to the recipient by the sender
+        emit Allocated(recipientId, amount, token, originSender);
+    }
+}
