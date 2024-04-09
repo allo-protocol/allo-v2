af7fc9ff21af5be73c1ef0dbe574863ccb9e48c2
Aditya Anand M C <aditya.anandmc@gmail.com>
2024-04-09 14:22:44 +0530

    wip

--- deploy/filecoin/00_deploy_FVMContractFactoy.ts
+++ deploy/filecoin/00_deploy_FVMContractFactoy.ts
@@ -1,49 +1,69 @@
-import * as dotenv from "dotenv";
-import "hardhat-deploy";
-import "hardhat-deploy-ethers";
-import { ethers } from "hardhat";
-import { Deployments } from "../../scripts/utils/scripts";
+import hre, { ethers } from "hardhat";
 
-dotenv.config();
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
 
-export default async ({ deployments }) => {
+import { Validator } from "../../scripts/utils/Validator";
+import { Deployments, delay, verifyContract } from "../../scripts/utils/scripts";
+
+module.exports = async ({ deployments }) => {
   const { deploy } = deployments;
-  const networkName = "filecoin-calibration";
-  const chainId = Number(314159);
+  const network = await ethers.provider.getNetwork();
+  const networkName = hre.network.name;
+  const chainId = Number(network.chainId);
+  const account = (await ethers.getSigners())[0];
+  const deployerAddress = await account.getAddress();
+  const blocksToWait = networkName === "localhost" ? 0 : 5;
+  const balance = await ethers.provider.getBalance(deployerAddress);
 
   const deploymentIo = new Deployments(chainId, "contractFactory");
 
   console.log(`
     ////////////////////////////////////////////////////
         Deploys ContractFactory.sol on ${networkName}
-    ////////////////////////////////////////////////////`);
-
-  console.log("Deploying ContractFactory.sol...");
-
-  // Filecoin Provider setup
-  const provider = new ethers.JsonRpcProvider(
-    "https://api.calibration.node.glif.io/rpc/v1"
-  );
-
-  const deployer = new ethers.Wallet(
-    process.env.DEPLOYER_PRIVATE_KEY,
-    ethers.provider
-  );
+    ////////////////////////////////////////////////////
+  `);
 
   console.table({
     contract: "Deploy ContractFactory.sol",
     chainId: chainId,
     network: networkName,
-    deployerAddress: deployer.address,
-    balance: ethers.formatEther(await provider.getBalance(deployer.address)),
+    deployerAddress: deployerAddress,
+    balance: ethers.formatEther(balance),
   });
 
-  // Deploy ContractFactory.sol
-  const ContractFactory = await deploy("ContractFactory", {
-    from: deployer.address,
+  console.log("Deploying ContractFactory.sol...");
+
+  const instance = await deploy("ContractFactory", {
+    from: deployerAddress,
     args: [],
     log: true,
+});
+
+  await instance.waitForDeployment();
+  await instance.deploymentTransaction()?.wait(blocksToWait);
+
+  console.log("ContractFactory deployed to:", instance.target);
+
+  const objToWrite = {
+    name: "ContractFactory",
+    address: instance.target,
+    deployerAddress: deployerAddress,
+  };
+
+  deploymentIo.write(objToWrite);
+  await delay(20000);
+  await verifyContract(instance.target.toString(), []);
+
+  const validator = await new Validator("ContractFactory", instance.target);
+
+  let result;
+  await validator.validate("isDeployer", [deployerAddress], "true").then(() => {
+    result = instance.target;
   });
 
-  console.log("ContractFactory deployed to:", ContractFactory.address);
-};
+}
+
+// Note: Deploy script to run in terminal:
+// npx hardhat compile --config config/fvm.hardhat.config.ts --network filecoin-calibration
+// npx hardhat deploy --config config/fvm.hardhat.config.ts --show-stack-traces --network filecoin-calibration
