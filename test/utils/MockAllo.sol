9436c6ef756db280f3eaee0d5b34b4a20eb44c2e
0xAustrian <austrian@defi.sucks>
2024-07-10 14:14:21 -0300

    fix: format

--- test/utils/MockAllo.sol
+++ test/utils/MockAllo.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.19;
 
-import { Allo } from "../../contracts/core/Allo.sol";
+import {Allo} from "../../contracts/core/Allo.sol";
 
 contract MockAllo is Allo {
     function mockMsgSender() external view returns (address) {
