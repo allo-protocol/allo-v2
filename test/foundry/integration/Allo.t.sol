93b03fff34aac279560664aeff742cdadb63f404
0xAustrian <austrian@defi.sucks>
2024-07-12 08:38:33 -0300

    test: adding working integration test

--- test/foundry/integration/Allo.t.sol
+++ test/foundry/integration/Allo.t.sol
@@ -4,21 +4,16 @@ pragma solidity ^0.8.19;
 import {Test, console} from "forge-std/Test.sol";
 import {Allo} from "../../../contracts/core/Allo.sol";
 import {Registry, Metadata} from "../../../contracts/core/Registry.sol";
-import {DonationVotingMerkleDistributionDirectTransferStrategy} from "../../../contracts/strategies/donation-voting-merkle-distribution-direct-transfer/DonationVotingMerkleDistributionDirectTransferStrategy.sol";
+import {DonationVotingMerkleDistributionDirectTransferStrategy} from
+    "../../../contracts/strategies/donation-voting-merkle-distribution-direct-transfer/DonationVotingMerkleDistributionDirectTransferStrategy.sol";
+import {DonationVotingMerkleDistributionBaseStrategy} from
+    "../../../contracts/strategies/donation-voting-merkle-base/DonationVotingMerkleDistributionBaseStrategy.sol";
 import {ISignatureTransfer} from "permit2/ISignatureTransfer.sol";
 import {IBiconomyForwarder} from "./IBiconomyForwarder.sol";
-import {ECDSA} from 'openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol';
+import {ECDSA} from "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 
 contract IntegrationAllo is Test {
-    struct InitializeData {
-        bool useRegistryAnchor;
-        bool metadataRequired;
-        uint64 registrationStartTime;
-        uint64 registrationEndTime;
-        uint64 allocationStartTime;
-        uint64 allocationEndTime;
-        address[] allowedTokens;
-    }
+    using ECDSA for bytes32;
 
     Allo public allo;
     Registry public registry;
@@ -27,7 +22,9 @@ contract IntegrationAllo is Test {
     address public owner;
     address public relayer;
     address public treasury;
-    address public user;
+    address public userAddr;
+
+    uint256 public userPk;
 
     bytes32 public profileId;
 
@@ -35,77 +32,89 @@ contract IntegrationAllo is Test {
     address public constant dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
 
     function setUp() public {
-        vm.createSelectFork(vm.rpcUrl('mainnet'));
+        vm.createSelectFork(vm.rpcUrl("mainnet"), 20289932);
 
-        owner = makeAddr('owner');
-        treasury = makeAddr('treasury');
-        user = makeAddr('user');
+        owner = makeAddr("owner");
+        treasury = makeAddr("treasury");
+        (userAddr, userPk) = makeAddrAndKey("user");
 
         allo = new Allo();
         registry = new Registry();
-        strategy = new DonationVotingMerkleDistributionDirectTransferStrategy(address(allo), "Test Strategy", ISignatureTransfer(address(1)));
-
-        allo.initialize(
-            owner,
-            address(registry),
-            payable(treasury),
-            0,
-            0,
-            biconomyForwarder
+        strategy = new DonationVotingMerkleDistributionDirectTransferStrategy(
+            address(allo), "Test Strategy", ISignatureTransfer(address(1))
         );
 
+        allo.initialize(owner, address(registry), payable(treasury), 0, 0, biconomyForwarder);
         registry.initialize(owner);
 
-        vm.prank(user);
-        profileId = registry.createProfile(0, "Test Profile", Metadata({
-            protocol: 1,
-            pointer: ''
-        }), user, new address[](0));
+        vm.prank(userAddr);
+        profileId =
+            registry.createProfile(0, "Test Profile", Metadata({protocol: 1, pointer: ""}), userAddr, new address[](0));
     }
 
     function test_CreatePoolWithMetaTx() public {
-        bytes memory _initStrategyData = abi.encode(InitializeData({
-            useRegistryAnchor: false,
-            metadataRequired: false,
-            registrationStartTime: uint64(block.timestamp), 
-            registrationEndTime: uint64(block.timestamp + 7 days),
-            allocationStartTime: uint64(block.timestamp),
-            allocationEndTime: uint64(block.timestamp + 7 days),
-            allowedTokens: new address[](0)
-        }));
+        bytes memory _initStrategyData = abi.encode(
+            DonationVotingMerkleDistributionBaseStrategy.InitializeData({
+                useRegistryAnchor: false,
+                metadataRequired: false,
+                registrationStartTime: uint64(block.timestamp),
+                registrationEndTime: uint64(block.timestamp + 7 days),
+                allocationStartTime: uint64(block.timestamp),
+                allocationEndTime: uint64(block.timestamp + 7 days),
+                allowedTokens: new address[](0)
+            })
+        );
 
         bytes32 domainSeparator = 0x4fde6db5140ab711910b567033f2d5e64dc4f7123d722004dd748edf6ed07abb; // Biconomy Forwarder
 
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(0x00, '');
+        // User signs the request
+        IBiconomyForwarder.ERC20ForwardRequest memory req = IBiconomyForwarder.ERC20ForwardRequest({
+            from: userAddr,
+            to: address(allo),
+            token: dai,
+            txGas: 500_000,
+            tokenGasPrice: 0,
+            batchId: 0,
+            batchNonce: 0,
+            deadline: block.timestamp + 1 days,
+            data: abi.encodeWithSelector(
+                allo.createPool.selector,
+                profileId,
+                address(strategy),
+                _initStrategyData,
+                dai,
+                0,
+                Metadata({protocol: 1, pointer: ""}),
+                new address[](0)
+            )
+        });
+        bytes32 structHash = keccak256(
+            abi.encode(
+                IBiconomyForwarder(biconomyForwarder).REQUEST_TYPEHASH(),
+                req.from,
+                req.to,
+                req.token,
+                req.txGas,
+                req.tokenGasPrice,
+                req.batchId,
+                0, // TODO: remove hardcoded nonce
+                req.deadline,
+                keccak256(req.data)
+            )
+        );
+        bytes32 digest = domainSeparator.toTypedDataHash(structHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPk, digest);
         bytes memory sig = abi.encodePacked(r, s, v);
 
+        // Relayer submits the request
         vm.prank(relayer);
-        IBiconomyForwarder(biconomyForwarder).executeEIP712(
-            IBiconomyForwarder.ERC20ForwardRequest({
-                from: user,
-                to: address(allo),
-                token: dai,
-                txGas: 0,
-                tokenGasPrice: 0,
-                batchId: 0,
-                batchNonce: 0,
-                deadline: block.timestamp + 1 days,
-                data: abi.encodeWithSelector(
-                    allo.createPool.selector,
-                    profileId,
-                    address(strategy),
-                    _initStrategyData,
-                    dai,
-                    0,
-                    Metadata({
-                        protocol: 1,
-                        pointer: ''
-                    }),
-                    new address[](0)
-                )
-            }),
-            domainSeparator,
-            sig
-        );
+        (bool success, bytes memory ret) =
+            IBiconomyForwarder(biconomyForwarder).executeEIP712(req, domainSeparator, sig);
+
+        // Check that the pool was created
+        uint256 poolId = abi.decode(ret, (uint256));
+        assertTrue(success);
+        assertTrue(allo.isPoolAdmin(poolId, userAddr));
+        assertFalse(allo.isPoolAdmin(poolId, relayer));
     }
 }
--- test/foundry/integration/IBiconomyForwarder.sol
+++ test/foundry/integration/IBiconomyForwarder.sol
@@ -3,20 +3,20 @@ pragma solidity 0.8.19;
 
 interface IBiconomyForwarder {
     struct ERC20ForwardRequest {
-        address from; 
-        address to; 
-        address token; 
+        address from;
+        address to;
+        address token;
         uint256 txGas;
         uint256 tokenGasPrice;
-        uint256 batchId; 
-        uint256 batchNonce; 
-        uint256 deadline; 
+        uint256 batchId;
+        uint256 batchNonce;
+        uint256 deadline;
         bytes data;
     }
 
-    function executeEIP712(
-        ERC20ForwardRequest calldata req,
-        bytes32 domainSeparator,
-        bytes calldata sig
-    ) external returns (bool success, bytes memory ret);
+    function executeEIP712(ERC20ForwardRequest calldata req, bytes32 domainSeparator, bytes calldata sig)
+        external
+        returns (bool success, bytes memory ret);
+
+    function REQUEST_TYPEHASH() external view returns (bytes32);
 }
