8136c0f726ea2bb176e2dd3d2fef5e5cd09c1f32
ilpepepig <ilpepepig@defi.sucks>
2024-07-27 17:25:15 -0300

    feat: addnew  rfp strategy

---
+++ contracts/strategies/RFPSimple.sol
@@ -0,0 +1,176 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.19;
+
+// Interfaces
+import {IAllo} from "../core/interfaces/IAllo.sol";
+// Core Contracts
+import {CoreBaseStrategy} from "./CoreBaseStrategy.sol";
+import {MilestonesExtension} from "../extensions/contracts/MilestonesExtension.sol";
+import {RecipientsExtension} from "../extensions/contracts/RecipientsExtension.sol";
+// Internal Libraries
+import {Metadata} from "../core/libraries/Metadata.sol";
+import {Errors} from "../core/libraries/Errors.sol";
+
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⢿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⡟⠘⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣿⣿⣿⣾⠻⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⡿⠀⠀⠸⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⢀⣠⣴⣴⣶⣶⣶⣦⣦⣀⡀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⡿⠃⠀⠙⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⠁⠀⠀⠀⢻⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠘⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⠃⠀⠀⠀⠀⠈⢿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⣰⣿⣿⣿⡿⠋⠁⠀⠀⠈⠘⠹⣿⣿⣿⣿⣆⠀⠀⠀
+// ⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⢰⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⡀⠀⠀
+// ⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣟⠀⡀⢀⠀⡀⢀⠀⡀⢈⢿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⠀⠀
+// ⠀⠀⣠⣿⣿⣿⣿⣿⣿⡿⠋⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⡿⢿⠿⠿⠿⠿⠿⠿⠿⠿⠿⢿⣿⣿⣿⣷⡀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠸⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⠂⠀⠀
+// ⠀⠀⠙⠛⠿⠻⠻⠛⠉⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣧⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⢻⣿⣿⣿⣷⣀⢀⠀⠀⠀⡀⣰⣾⣿⣿⣿⠏⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣧⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠹⢿⣿⣿⣿⣿⣾⣾⣷⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠙⠋⠛⠙⠋⠛⠙⠋⠛⠙⠋⠃⠀⠀⠀⠀⠀⠀⠀⠀⠠⠿⠻⠟⠿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠟⠿⠟⠿⠆⠀⠸⠿⠿⠟⠯⠀⠀⠀⠸⠿⠿⠿⠏⠀⠀⠀⠀⠀⠈⠉⠻⠻⡿⣿⢿⡿⡿⠿⠛⠁⠀⠀⠀⠀⠀⠀
+//                    allo.gitcoin.co
+
+/// @title RFP Simple Strategy
+/// @notice Strategy for Request for Proposal (RFP) allocation with milestone submission and management.
+contract RFPSimple is CoreBaseStrategy, MilestonesExtension, RecipientsExtension {
+    /// ================================
+    /// ========== Struct ==============
+    /// ================================
+
+    /// @notice Stores the details needed for initializing strategy
+    struct InitializeParams {
+        IRecipientsExtension.RecipientInitializeData recipientExtensionInitializeData;
+        IMilestonesExtension.InitializeParams milestonesExtensionInitializeParams;
+    }
+
+    /// ================================
+    /// ========== Storage =============
+    /// ================================
+
+    mapping(uint256 => bool) public wasMilestonePaid;
+
+    /// ===============================
+    /// ======== Constructor ==========
+    /// ===============================
+
+    /// @notice Constructor for the RFP Simple Strategy
+    /// @param _allo The 'Allo' contract
+    constructor(address _allo) CoreBaseStrategy(_allo) {}
+
+    /// ===============================
+    /// ========= Initialize ==========
+    /// ===============================
+
+    // @notice Initialize the strategy
+    /// @param _poolId ID of the pool
+    /// @param _data The data to be decoded
+    /// @custom:data (uint256 _maxBid, bool registryGating, bool metadataRequired)
+    function initialize(uint256 _poolId, bytes memory _data) external virtual override {
+        (
+            IRecipientsExtension.RecipientInitializeData memory _recipientExtensionInitializeData,
+            IMilestonesExtension.InitializeParams memory _milestonesExtensionInitializeParams
+        ) = abi.decode(_data, (IRecipientsExtension.RecipientInitializeData, IMilestonesExtension.InitializeParams));
+        __BaseStrategy_init(_poolId);
+        __RecipientsExtension_init(_recipientExtensionInitializeData);
+        __MilestonesExtension_init(_milestonesExtensionInitializeParams);
+        emit Initialized(_poolId, _data);
+    }
+
+    /// ===============================
+    /// ======= External/Custom =======
+    /// ===============================
+
+    function reviewRecipients(ApplicationStatus[] memory statuses, uint256 refRecipientsCounter)
+        public
+        override
+        onlyActiveRegistration
+    {
+        bool recipientAccepted;
+        for (uint256 i; i < statuses.length;) {
+            uint256 fullRow = statuses[i].statusRow;
+            for (uint256 col = 0; col < 64;) {
+                uint8 status = uint8((fullRow >> col * 4) & 0xFF);
+                if (IRecipientsExtension.Status(status) == IRecipientsExtension.Status.Accepted) {
+                    if (recipientAccepted) {
+                        // Only one recipient can be accepted
+                        revert INVALID();
+                    } else {
+                        recipientAccepted = true;
+                        registrationEndTime = block.timestamp - 1;
+                    }
+                }
+                unchecked {
+                    col++;
+                }
+            }
+            unchecked {
+                i++;
+            }
+        }
+        super.reviewRecipients(statuses, refRecipientsCounter);
+    }
+
+    /// ====================================
+    /// ============ Internal ==============
+    /// ====================================
+
+    /// @notice Hook to process recipient data
+    /// @param _recipientId ID of the recipient
+    /// @param _isUsingRegistryAnchor A flag to indicate whether to use the registry anchor or not
+    /// @param _metadata The metadata of the recipient
+    /// @param _extraData The extra data of the recipient
+    function _processRecipient(address _recipientId, bool, Metadata memory, bytes memory _extraData)
+        internal
+        virtual
+        override
+    {
+        uint256 proposalBid = abi.decode(_extraData, (uint256));
+        _setProposalBid(_recipientId, proposalBid);
+    }
+
+    /// @notice Distributes funds (tokens) to recipients.
+    /// @dev The encoded '_data' will be determined by the strategy implementation. Only 'Allo' contract can
+    ///      call this when it is initialized.
+    /// @param _recipientIds The IDs of the recipients
+    /// @param _data The data to use to distribute to the recipients
+    /// @param _sender The address of the sender
+    function _distribute(address[] memory _recipientIds, bytes memory _data, address _sender)
+        internal
+        virtual
+        override
+    {
+        if (block.timestamp <= registrationEndTime) {
+            revert Errors.ALLOCATION_NOT_ENDED();
+        }
+        if (_recipientIds.length > 1) {
+            revert Errors.RECIPIENT_NOT_ACCEPTED();
+        }
+
+        uint256 acceptedRecipientId = _recipientIds[0];
+        uint256[] memory milestoneIds = abi.decode(_data, (uint256[]));
+        uint256 amount;
+        for (uint256 i = 0; i < milestoneIds.length; i++) {
+            Milestone storage milestone = milestones[milestoneId];
+
+            // Check if the milestone is pending
+            if (milestone.milestoneStatus != Status.Accepted || wasMilestonePaid[milestoneId]) {
+                revert IMilestonesExtension.MilestonesExtension_INVALID_MILESTONE_STATUS();
+            }
+
+            // Calculate the amount to be distributed for the milestone
+            // Reverts if the recipient is not actually accepted
+            amount += _getMilestonePayout(acceptedRecipientId, milestoneId);
+            wasMilestonePaid[milestoneId] = true;
+        }
+        poolAmount -= amount;
+
+        IAllo.Pool memory pool = allo.getPool(poolId);
+        address recipientAddress = _recipients[acceptedRecipientId].recipientAddress;
+        _transferAmount(pool.token, recipientAddress, amount);
+
+        emit Distributed(acceptedRecipientId, recipientAddress, amount, _sender);
+    }
+
+    /// @notice Returns if the recipient is accepted
+    /// @param _recipientId The recipient id
+    /// @return If the recipient is accepted
+    function _isAcceptedRecipient(address _recipientId) internal view virtual override returns (bool) {
+        return _getRecipientStatus(_recipientId) == IRecipientsExtension.Status.Accepted;
+    }
+}
