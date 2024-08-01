f074bf0e71cfa2864097412347fae701d5c0f079
ilpepepig <ilpepepig@defi.sucks>
2024-08-01 14:56:19 -0300

    feat: add donation voting strategy

---
+++ contracts/strategies/DonationVoting.sol
@@ -0,0 +1,327 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.19;
+
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
+// Interfaces
+import {IAllo} from "../core/interfaces/IAllo.sol";
+import {IRecipientsExtension} from "../extensions/interfaces/IRecipientsExtension.sol";
+// Core Contracts
+import {CoreBaseStrategy} from "./CoreBaseStrategy.sol";
+import {RecipientsExtension} from "../extensions/contracts/RecipientsExtension.sol";
+// Internal Libraries
+import {Errors} from "../core/libraries/Errors.sol";
+import {Native} from "../core/libraries/Native.sol";
+
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⢿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⡟⠘⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣿⣿⣿⣾⠻⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⡿⠀⠀⠸⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⢀⣠⣴⣴⣶⣶⣶⣦⣦⣀⡀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⡿⠃⠀⠙⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⠁⠀⠀⠀⢻⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠘⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⠃⠀⠀⠀⠀⠈⢿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⣰⣿⣿⣿⡿⠋⠁⠀⠀⠈⠘⠹⣿⣿⣿⣿⣆⠀⠀⠀
+// ⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⢰⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⡀⠀⠀
+// ⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣟⠀⡀⢀⠀⡀⢀⠀⡀⢈⢿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⠀⠀
+// ⠀⠀⣠⣿⣿⣿⣿⣿⣿⡿⠋⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⡿⢿⠿⠿⠿⠿⠿⠿⠿⠿⠿⢿⣿⣿⣿⣷⡀⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠸⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⠂⠀⠀
+// ⠀⠀⠙⠛⠿⠻⠻⠛⠉⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣧⠀⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⢻⣿⣿⣿⣷⣀⢀⠀⠀⠀⡀⣰⣾⣿⣿⣿⠏⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣧⠀⠀⢸⣿⣿⣿⣗⠀⠀⠀⢸⣿⣿⣿⡯⠀⠀⠀⠀⠹⢿⣿⣿⣿⣿⣾⣾⣷⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠙⠋⠛⠙⠋⠛⠙⠋⠛⠙⠋⠃⠀⠀⠀⠀⠀⠀⠀⠀⠠⠿⠻⠟⠿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠟⠿⠟⠿⠆⠀⠸⠿⠿⠟⠯⠀⠀⠀⠸⠿⠿⠿⠏⠀⠀⠀⠀⠀⠈⠉⠻⠻⡿⣿⢿⡿⡿⠿⠛⠁⠀⠀⠀⠀⠀⠀
+//                    allo.gitcoin.co
+
+/// @title RFP Simple Strategy
+/// @notice Strategy for Request for Proposal (RFP) allocation with milestone submission and management.
+contract DonationVoting is CoreBaseStrategy, RecipientsExtension {
+    /// ================================
+    /// ========== Struct ==============
+    /// ================================
+
+    /// @notice Payout summary struct to hold the payout data
+    struct PayoutSummary {
+        address recipientAddress;
+        uint256 amount;
+    }
+
+    /// @notice Struct to hold details of the allocations to claim
+    struct Claim {
+        address recipientId;
+        address token;
+    }
+
+    /// ================================
+    /// ========== Storage =============
+    /// ================================
+
+    /// @notice The start and end times for allocations
+    uint64 public allocationStartTime;
+    uint64 public allocationEndTime;
+    uint64 public withdrawalCooldown;
+
+    uint256 public totalPayoutAmount;
+
+    /// @notice token -> bool
+    mapping(address => bool) public allowedTokens;
+    /// @notice recipientId -> PayoutSummary
+    mapping(address => PayoutSummary) public payoutSummaries;
+    /// @notice recipientId -> token -> amount
+    mapping(address => mapping(address => uint256)) public claims;
+
+    /// ===============================
+    /// ========== Events =============
+    /// ===============================
+
+    event Claimed(address indexed recipientId, address recipientAddress, uint256 amount, address token);
+    event AllocationTimestampsUpdated(uint64 allocationStartTime, uint64 allocationEndTime, address sender);
+    event PayoutSet(bytes recipientIds);
+
+    /// ================================
+    /// ========== Modifier ============
+    /// ================================
+
+    /// @notice Modifier to check if allocation is active
+    /// @dev Reverts if allocation is not active
+    modifier onlyActiveAllocation() {
+        if (allocationStartTime > block.timestamp || block.timestamp > allocationEndTime) {
+            revert ALLOCATION_NOT_ACTIVE();
+        }
+        _;
+    }
+
+    /// @notice Modifier to check if allocation has ended
+    /// @dev Reverts if allocation has not ended
+    modifier onlyAfterAllocation() {
+        if (block.timestamp <= allocationEndTime) {
+            revert ALLOCATION_NOT_ENDED();
+        }
+        _;
+    }
+
+    /// ===============================
+    /// ======== Constructor ==========
+    /// ===============================
+
+    /// @notice Constructor for the RFP Simple Strategy
+    /// @param _allo The 'Allo' contract
+    constructor(address _allo) CoreBaseStrategy(_allo) {}
+
+    /// ===============================
+    /// ========= Initialize ==========
+    /// ===============================
+
+    // @notice Initialize the strategy
+    /// @param _poolId ID of the pool
+    /// @param _data The data to be decoded
+    /// @custom:data (uint256 _maxBid, bool registryGating, bool metadataRequired)
+    function initialize(uint256 _poolId, bytes memory _data) external virtual override {
+        (
+            IRecipientsExtension.RecipientInitializeData memory _recipientExtensionInitializeData,
+            uint64 _allocationStartTime,
+            uint64 _allocationEndTime,
+            uint64 _withdrawalCooldown,
+            address[] memory _allowedTokens
+        ) = abi.decode(_data, (IRecipientsExtension.RecipientInitializeData, uint64, uint64, uint64, address[]));
+        __BaseStrategy_init(_poolId);
+        __RecipientsExtension_init(_recipientExtensionInitializeData);
+
+        _isPoolTimestampValid(_allocationStartTime, _allocationEndTime);
+        allocationStartTime = _allocationStartTime;
+        allocationEndTime = _allocationEndTime;
+        emit AllocationTimestampsUpdated(_allocationStartTime, _allocationEndTime, msg.sender);
+
+        withdrawalCooldown = _withdrawalCooldown;
+
+        if (_allowedTokens.length == 0) {
+            // all tokens
+            allowedTokens[address(0)] = true;
+        } else {
+            for (uint256 i; i < _allowedTokens.length; i++) {
+                allowedTokens[_allowedTokens[i]] = true;
+            }
+        }
+
+        emit Initialized(_poolId, _data);
+    }
+
+    /// ===============================
+    /// ======= External/Custom =======
+    /// ===============================
+
+    function reviewRecipients(ApplicationStatus[] memory statuses, uint256 refRecipientsCounter)
+        public
+        override
+        onlyActiveRegistration
+    {
+        super.reviewRecipients(statuses, refRecipientsCounter);
+    }
+
+    /// @notice Claim allocated tokens
+    /// @param _claims Claims to be claimed
+    function claimAllocation(Claim[] calldata _claims) external onlyAfterAllocation {
+        uint256 claimsLength = _claims.length;
+        for (uint256 i; i < claimsLength; i++) {
+            Claim calldata claim = _claims[i];
+            uint256 amount = claims[claim.recipientId][claim.token];
+
+            if (amount == 0) {
+                revert INVALID();
+            }
+
+            delete claims[claim.recipientId][claim.token];
+
+            address recipientAddress = _recipients[claim.recipientId].recipientAddress;
+            _transferAmount(claim.token, recipientAddress, amount);
+
+            emit Claimed(claim.recipientId, recipientAddress, amount, claim.token);
+        }
+    }
+
+    /// @notice Set payout for the recipients
+    /// @param _recipientIds Ids of the recipients
+    /// @param _amounts Amounts to be paid out
+    function setPayout(address[] memory _recipientIds, uint256[] memory _amounts)
+        external
+        onlyPoolManager(msg.sender)
+        onlyAfterAllocation
+    {
+        for (uint256 i; i < _recipientIds.length; i++) {
+            address recipientId = _recipientIds[i];
+            if (!_isAcceptedRecipient(_recipientIds[i])) {
+                revert RECIPIENT_ERROR(_recipientIds[i]);
+            }
+
+            PayoutSummary storage payoutSummary = payoutSummaries[recipientId];
+            if (payoutSummary.amount != 0) {
+                revert RECIPIENT_ERROR(recipientId);
+            }
+
+            uint256 amount = _amounts[i];
+            totalPayoutAmount += amount;
+
+            if (totalPayoutAmount > poolAmount) {
+                revert INVALID();
+            }
+
+            payoutSummary.amount = amount;
+            payoutSummary.recipientAddress = _recipients[recipientId].recipientAddress;
+        }
+
+        emit PayoutSet(abi.encode(_recipientIds));
+    }
+
+    /// ====================================
+    /// ============ Internal ==============
+    /// ====================================
+
+    /// @notice This will allocate to recipients.
+    /// @dev The encoded '_data' will be determined by the strategy implementation.
+    /// @param _recipients The addresses of the recipients to allocate to
+    /// @param _amounts The amounts to allocate to the recipients
+    /// @param _data The data to use to allocate to the recipient
+    /// @param _sender The address of the sender
+    function _allocate(address[] memory _recipients, uint256[] memory _amounts, bytes memory _data, address _sender)
+        internal
+        virtual
+        override
+        onlyActiveAllocation
+    {
+        (address[] memory tokens) = abi.decode(_data, (address[]));
+        uint256 totalNativeAmount;
+
+        for (uint256 i = 0; i < _recipients.length; i++) {
+            if (!_isAcceptedRecipient(_recipients[i])) {
+                revert RECIPIENT_ERROR(_recipients[i]);
+            }
+
+            if (!allowedTokens[tokens[i]] && !allowedTokens[address(0)]) {
+                revert INVALID();
+            }
+
+            // Update the total payout amount for the claim and the total claimable amount
+            claims[_recipients[i]][tokens[i]] += _amounts[i];
+
+            if (tokens[i] == NATIVE) {
+                totalNativeAmount += _amounts[i];
+            } else {
+                SafeTransferLib.safeTransferFrom(tokens[i], _sender, address(this), _amounts[i]);
+            }
+
+            emit Allocated(_recipients[i], _sender, _amounts[i], abi.encode(tokens[i]));
+        }
+
+        if (msg.value != totalNativeAmount) {
+            revert AMOUNT_MISMATCH();
+        }
+    }
+
+    /// @notice Distributes funds (tokens) to recipients.
+    /// @dev The encoded '_data' will be determined by the strategy implementation. Only 'Allo' contract can
+    ///      call this when it is initialized.
+    /// @param _recipientIds The IDs of the recipients
+    /// @param _sender The address of the sender
+    function _distribute(address[] memory _recipientIds, bytes memory, address _sender)
+        internal
+        virtual
+        override
+        onlyPoolManager(_sender)
+        onlyAfterAllocation
+    {
+        for (uint256 i; i < _recipientIds.length; i++) {
+            address recipientId = _recipientIds[i];
+
+            uint256 amount = payoutSummaries[recipientId].amount;
+            delete payoutSummaries[recipientId].amount;
+
+            if (amount == 0) {
+                revert INVALID();
+            }
+            poolAmount -= amount;
+
+            address recipientAddress = _recipients[recipientId].recipientAddress;
+            IAllo.Pool memory pool = allo.getPool(poolId);
+            _transferAmount(pool.token, recipientAddress, amount);
+
+            emit Distributed(recipientId, abi.encode(recipientAddress, amount, _sender));
+        }
+    }
+
+    /// @notice Hook called before withdrawing tokens from the pool.
+    /// @param _amount The amount to withdraw
+    function _beforeWithdraw(address, uint256 _amount, address) internal virtual override {
+        if (block.timestamp <= allocationEndTime + withdrawalCooldown) {
+            revert INVALID();
+        }
+
+        if (poolAmount - totalPayoutAmount < _amount) {
+            revert INVALID();
+        }
+    }
+
+    /// @notice Set the start and end dates for the pool
+    /// @param _allocationStartTime The start time for the allocation
+    /// @param _allocationEndTime The end time for the allocation
+    function updateAllocationTimestamps(uint64 _allocationStartTime, uint64 _allocationEndTime)
+        external
+        onlyPoolManager(msg.sender)
+    {
+        _isAllocationTimestampValid(_allocationStartTime, _allocationEndTime);
+
+        allocationStartTime = _allocationStartTime;
+        allocationEndTime = _allocationEndTime;
+
+        emit AllocationTimestampsUpdated(allocationStartTime, allocationEndTime, msg.sender);
+    }
+
+    function _isAllocationTimestampValid(uint64 _allocationStartTime, uint64 _allocationEndTime) internal view {
+        if (
+            block.timestamp > registrationStartTime || registrationStartTime > _allocationStartTime
+                || _allocationStartTime > _allocationEndTime || registrationEndTime > _allocationEndTime
+        ) {
+            revert INVALID();
+        }
+    }
+
+    /// @notice Returns if the recipient is accepted
+    /// @param _recipientId The recipient id
+    /// @return If the recipient is accepted
+    function _isAcceptedRecipient(address _recipientId) internal view virtual returns (bool) {
+        return _getRecipientStatus(_recipientId) == IRecipientsExtension.Status.Accepted;
+    }
+}
