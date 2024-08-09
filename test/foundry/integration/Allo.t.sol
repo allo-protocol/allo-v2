552a5b0a94d35c9f51194db273dbb68d0bb453ff
0xAustrian <austrian@defi.sucks>
2024-08-09 09:13:24 -0300

    test: move logic to Base

--- test/foundry/integration/Allo.t.sol
+++ test/foundry/integration/Allo.t.sol
@@ -2,64 +2,18 @@
 pragma solidity ^0.8.19;
 
 import {IAllo} from "contracts/core/interfaces/IAllo.sol";
-import {Metadata, IRegistry} from "contracts/core/Registry.sol";
+import {Metadata} from "contracts/core/Registry.sol";
 import {DonationVotingMerkleDistributionDirectTransferStrategy} from
     "contracts/strategies/donation-voting-merkle-distribution-direct-transfer/DonationVotingMerkleDistributionDirectTransferStrategy.sol";
 import {DonationVotingMerkleDistributionBaseStrategy} from
     "contracts/strategies/donation-voting-merkle-base/DonationVotingMerkleDistributionBaseStrategy.sol";
 import {ISignatureTransfer} from "permit2/ISignatureTransfer.sol";
-import {IBiconomyForwarder} from "./IBiconomyForwarder.sol";
-import {ECDSA} from "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {IntegrationBase} from "./IntegrationBase.sol";
 
 contract IntegrationAllo is IntegrationBase {
-    using ECDSA for bytes32;
-
     IAllo public allo;
     DonationVotingMerkleDistributionDirectTransferStrategy public strategy;
-
-    address public relayer;
-    address public userAddr;
-
-    address public recipient0Addr;
-    address public recipient1Addr;
-    address public recipient2Addr;
-
-    uint256 public userPk;
-    uint256 public recipient0Pk;
-    uint256 public recipient1Pk;
-    uint256 public recipient2Pk;
-
-    bytes32 public profileId;
-
-    address public constant dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-
-    /// @notice Biconomy Forwarder separator
-    bytes32 public constant domainSeparator = 0x4fde6db5140ab711910b567033f2d5e64dc4f7123d722004dd748edf6ed07abb;
-
-    function setUp() public override {
-        super.setUp();
-
-        allo = IAllo(ALLO_PROXY);
-
-        (userAddr, userPk) = makeAddrAndKey("user");
-        (recipient0Addr, recipient0Pk) = makeAddrAndKey("recipient0");
-        (recipient1Addr, recipient1Pk) = makeAddrAndKey("recipient1");
-        (recipient2Addr, recipient2Pk) = makeAddrAndKey("recipient2");
-
-        strategy = new DonationVotingMerkleDistributionDirectTransferStrategy(
-            address(allo), "Test Strategy", ISignatureTransfer(address(1))
-        );
-
-        vm.prank(userAddr);
-        profileId = IRegistry(registry).createProfile(
-            0, "Test Profile", Metadata({protocol: 1, pointer: ""}), userAddr, new address[](0)
-        );
-
-        // Deal 130k DAI to the user
-        deal(dai, userAddr, 130_000 ether);
-    }
 
     function _getApplicationStatus(address _recipientId, uint256 _status, address payable _strategy)
         internal
@@ -78,45 +32,17 @@ contract IntegrationAllo is IntegrationBase {
         return DonationVotingMerkleDistributionBaseStrategy.ApplicationStatus({index: rowIndex, statusRow: statusRow});
     }
 
-    /// @dev Send a transaction using the Biconomy Forwarder
-    function _sendWithRelayer(address _from, address _to, bytes memory _data, uint256 _userPk)
-        internal
-        returns (bool _success, bytes memory _returnData)
-    {
-        // User signs the request
-        IBiconomyForwarder.ERC20ForwardRequest memory req = IBiconomyForwarder.ERC20ForwardRequest({
-            from: _from,
-            to: _to,
-            token: address(0),
-            txGas: 500_000,
-            tokenGasPrice: 0,
-            batchId: 0,
-            batchNonce: 0,
-            deadline: block.timestamp + 1 days,
-            data: _data
-        });
-        bytes32 structHash = keccak256(
-            abi.encode(
-                IBiconomyForwarder(BICONOMY_FORWARDER).REQUEST_TYPEHASH(),
-                req.from,
-                req.to,
-                req.token,
-                req.txGas,
-                req.tokenGasPrice,
-                req.batchId,
-                IBiconomyForwarder(BICONOMY_FORWARDER).getNonce(req.from, req.batchId),
-                req.deadline,
-                keccak256(req.data)
-            )
-        );
-        bytes32 digest = domainSeparator.toTypedDataHash(structHash);
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_userPk, digest);
-        bytes memory sig = abi.encodePacked(r, s, v);
+    function setUp() public override {
+        super.setUp();
 
-        // Relayer submits the request
-        vm.prank(relayer);
-        (_success, _returnData) = IBiconomyForwarder(BICONOMY_FORWARDER).executeEIP712(req, domainSeparator, sig);
-        assertTrue(_success);
+        allo = IAllo(ALLO_PROXY);
+
+        strategy = new DonationVotingMerkleDistributionDirectTransferStrategy(
+            ALLO_PROXY, "Test Strategy", ISignatureTransfer(address(1))
+        );
+
+        // Deal 130k DAI to the user
+        deal(DAI, userAddr, 130_000 ether);
     }
 
     /// @dev Test the full flow, using meta-tx when possible:
@@ -128,7 +54,7 @@ contract IntegrationAllo is IntegrationBase {
     function test_fullFlowWithMetaTx() public {
         // Create pool
         address[] memory _allowedTokens = new address[](1);
-        _allowedTokens[0] = dai;
+        _allowedTokens[0] = DAI;
 
         bytes memory _initStrategyData = abi.encode(
             DonationVotingMerkleDistributionBaseStrategy.InitializeData({
@@ -150,7 +76,7 @@ contract IntegrationAllo is IntegrationBase {
                 profileId,
                 address(strategy),
                 _initStrategyData,
-                dai,
+                DAI,
                 0,
                 Metadata({protocol: 1, pointer: ""}),
                 new address[](0)
@@ -167,14 +93,14 @@ contract IntegrationAllo is IntegrationBase {
 
         // Fund pool
         vm.prank(userAddr);
-        IERC20(dai).approve(address(allo), 100_000 ether);
+        IERC20(DAI).approve(address(allo), 100_000 ether);
 
         _sendWithRelayer(
             userAddr, address(allo), abi.encodeWithSelector(allo.fundPool.selector, poolId, 100_000 ether), userPk
         );
         // userAddr transferred 100k DAI to the pool
-        assertTrue(IERC20(dai).balanceOf(address(deployedStrategy)) == 100_000 ether);
-        assertTrue(IERC20(dai).balanceOf(userAddr) == 30_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(address(deployedStrategy)) == 100_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(userAddr) == 30_000 ether);
 
         // Register recipients
         _sendWithRelayer(
@@ -231,7 +157,7 @@ contract IntegrationAllo is IntegrationBase {
 
         // Allocate
         vm.prank(userAddr);
-        IERC20(dai).approve(address(deployedStrategy), 30_000 ether);
+        IERC20(DAI).approve(address(deployedStrategy), 30_000 ether);
 
         _sendWithRelayer(
             userAddr,
@@ -244,7 +170,7 @@ contract IntegrationAllo is IntegrationBase {
                     DonationVotingMerkleDistributionBaseStrategy.PermitType.None,
                     DonationVotingMerkleDistributionBaseStrategy.Permit2Data({
                         permit: ISignatureTransfer.PermitTransferFrom({
-                            permitted: ISignatureTransfer.TokenPermissions({token: dai, amount: 10_000 ether}),
+                            permitted: ISignatureTransfer.TokenPermissions({token: DAI, amount: 10_000 ether}),
                             nonce: 0,
                             deadline: 0
                         }),
@@ -265,7 +191,7 @@ contract IntegrationAllo is IntegrationBase {
                     DonationVotingMerkleDistributionBaseStrategy.PermitType.None,
                     DonationVotingMerkleDistributionBaseStrategy.Permit2Data({
                         permit: ISignatureTransfer.PermitTransferFrom({
-                            permitted: ISignatureTransfer.TokenPermissions({token: dai, amount: 10_000 ether}),
+                            permitted: ISignatureTransfer.TokenPermissions({token: DAI, amount: 10_000 ether}),
                             nonce: 0,
                             deadline: 0
                         }),
@@ -276,13 +202,13 @@ contract IntegrationAllo is IntegrationBase {
             userPk
         );
         // Strategy still has 100k DAI, userAddr has 10k DAI
-        assertTrue(IERC20(dai).balanceOf(address(deployedStrategy)) == 100_000 ether);
-        assertTrue(IERC20(dai).balanceOf(userAddr) == 10_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(address(deployedStrategy)) == 100_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(userAddr) == 10_000 ether);
         // Recipients 0 and 1 have 10k DAI each
-        assertTrue(IERC20(dai).balanceOf(recipient0Addr) == 10_000 ether);
-        assertTrue(IERC20(dai).balanceOf(recipient1Addr) == 10_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(recipient0Addr) == 10_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(recipient1Addr) == 10_000 ether);
         // Recipient 2 has 0 DAI
-        assertTrue(IERC20(dai).balanceOf(recipient2Addr) == 0 ether);
+        assertTrue(IERC20(DAI).balanceOf(recipient2Addr) == 0 ether);
 
         // Move time after allocation end time
         vm.warp(block.timestamp + 8 days);
@@ -332,10 +258,10 @@ contract IntegrationAllo is IntegrationBase {
             userPk
         );
         // After distribution, the strategy has 10k DAI, recipients have 35k, 40k, and 35k DAI
-        assertTrue(IERC20(dai).balanceOf(address(deployedStrategy)) == 10_000 ether);
-        assertTrue(IERC20(dai).balanceOf(recipient0Addr) == 35_000 ether);
-        assertTrue(IERC20(dai).balanceOf(recipient1Addr) == 40_000 ether);
-        assertTrue(IERC20(dai).balanceOf(recipient2Addr) == 35_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(address(deployedStrategy)) == 10_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(recipient0Addr) == 35_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(recipient1Addr) == 40_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(recipient2Addr) == 35_000 ether);
     }
 
     /// @dev Test the full flow:
@@ -347,7 +273,7 @@ contract IntegrationAllo is IntegrationBase {
     function test_fullFlow() public {
         // Create pool
         address[] memory _allowedTokens = new address[](1);
-        _allowedTokens[0] = dai;
+        _allowedTokens[0] = DAI;
 
         bytes memory _initStrategyData = abi.encode(
             DonationVotingMerkleDistributionBaseStrategy.InitializeData({
@@ -366,7 +292,7 @@ contract IntegrationAllo is IntegrationBase {
             profileId,
             address(strategy),
             _initStrategyData,
-            dai,
+            DAI,
             0,
             Metadata({protocol: 1, pointer: ""}),
             new address[](0)
@@ -379,12 +305,12 @@ contract IntegrationAllo is IntegrationBase {
             DonationVotingMerkleDistributionDirectTransferStrategy(payable(address(allo.getPool(poolId).strategy)));
 
         // Fund pool
-        IERC20(dai).approve(address(allo), 100_000 ether);
+        IERC20(DAI).approve(address(allo), 100_000 ether);
         allo.fundPool(poolId, 100_000 ether);
 
         // userAddr transferred 100k DAI to the pool
-        assertTrue(IERC20(dai).balanceOf(address(deployedStrategy)) == 100_000 ether);
-        assertTrue(IERC20(dai).balanceOf(userAddr) == 30_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(address(deployedStrategy)) == 100_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(userAddr) == 30_000 ether);
 
         vm.stopPrank();
 
@@ -418,7 +344,7 @@ contract IntegrationAllo is IntegrationBase {
         deployedStrategy.reviewRecipients(statuses, deployedStrategy.recipientsCounter());
 
         // Allocate
-        IERC20(dai).approve(address(deployedStrategy), 30_000 ether);
+        IERC20(DAI).approve(address(deployedStrategy), 30_000 ether);
 
         allo.allocate(
             poolId,
@@ -427,7 +353,7 @@ contract IntegrationAllo is IntegrationBase {
                 DonationVotingMerkleDistributionBaseStrategy.PermitType.None,
                 DonationVotingMerkleDistributionBaseStrategy.Permit2Data({
                     permit: ISignatureTransfer.PermitTransferFrom({
-                        permitted: ISignatureTransfer.TokenPermissions({token: dai, amount: 10_000 ether}),
+                        permitted: ISignatureTransfer.TokenPermissions({token: DAI, amount: 10_000 ether}),
                         nonce: 0,
                         deadline: 0
                     }),
@@ -442,7 +368,7 @@ contract IntegrationAllo is IntegrationBase {
                 DonationVotingMerkleDistributionBaseStrategy.PermitType.None,
                 DonationVotingMerkleDistributionBaseStrategy.Permit2Data({
                     permit: ISignatureTransfer.PermitTransferFrom({
-                        permitted: ISignatureTransfer.TokenPermissions({token: dai, amount: 10_000 ether}),
+                        permitted: ISignatureTransfer.TokenPermissions({token: DAI, amount: 10_000 ether}),
                         nonce: 0,
                         deadline: 0
                     }),
@@ -451,13 +377,13 @@ contract IntegrationAllo is IntegrationBase {
             )
         );
         // Strategy still has 100k DAI, userAddr has 10k DAI
-        assertTrue(IERC20(dai).balanceOf(address(deployedStrategy)) == 100_000 ether);
-        assertTrue(IERC20(dai).balanceOf(userAddr) == 10_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(address(deployedStrategy)) == 100_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(userAddr) == 10_000 ether);
         // Recipients 0 and 1 have 10k DAI each
-        assertTrue(IERC20(dai).balanceOf(recipient0Addr) == 10_000 ether);
-        assertTrue(IERC20(dai).balanceOf(recipient1Addr) == 10_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(recipient0Addr) == 10_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(recipient1Addr) == 10_000 ether);
         // Recipient 2 has 0 DAI
-        assertTrue(IERC20(dai).balanceOf(recipient2Addr) == 0 ether);
+        assertTrue(IERC20(DAI).balanceOf(recipient2Addr) == 0 ether);
 
         // Move time after allocation end time
         vm.warp(block.timestamp + 8 days);
@@ -502,10 +428,10 @@ contract IntegrationAllo is IntegrationBase {
         allo.distribute(poolId, new address[](0), _distributeData);
 
         // After distribution, the strategy has 10k DAI, recipients have 35k, 40k, and 35k DAI
-        assertTrue(IERC20(dai).balanceOf(address(deployedStrategy)) == 10_000 ether);
-        assertTrue(IERC20(dai).balanceOf(recipient0Addr) == 35_000 ether);
-        assertTrue(IERC20(dai).balanceOf(recipient1Addr) == 40_000 ether);
-        assertTrue(IERC20(dai).balanceOf(recipient2Addr) == 35_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(address(deployedStrategy)) == 10_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(recipient0Addr) == 35_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(recipient1Addr) == 40_000 ether);
+        assertTrue(IERC20(DAI).balanceOf(recipient2Addr) == 35_000 ether);
 
         vm.stopPrank();
     }
--- test/foundry/integration/IntegrationBase.sol
+++ test/foundry/integration/IntegrationBase.sol
@@ -5,21 +5,89 @@ import {Test} from "forge-std/Test.sol";
 import {Allo, IAllo} from "contracts/core/Allo.sol";
 import {ITransparentUpgradeableProxy} from "./ITransparentUpgradeableProxy.sol";
 import {IOwnable} from "./IOwnable.sol";
+import {ISignatureTransfer} from "permit2/ISignatureTransfer.sol";
+import {IBiconomyForwarder} from "./IBiconomyForwarder.sol";
+import {ECDSA} from "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+import {Metadata, IRegistry} from "contracts/core/Registry.sol";
 
 abstract contract IntegrationBase is Test {
+    using ECDSA for bytes32;
+
     address public constant ALLO_PROXY = 0x1133eA7Af70876e64665ecD07C0A0476d09465a1;
     address public constant BICONOMY_FORWARDER = 0x84a0856b038eaAd1cC7E297cF34A7e72685A8693;
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+
+    bytes32 public constant DOMAIN_SEPARATOR = 0x4fde6db5140ab711910b567033f2d5e64dc4f7123d722004dd748edf6ed07abb;
+
+    address public userAddr;
+    address public recipient0Addr;
+    address public recipient1Addr;
+    address public recipient2Addr;
+    
+    uint256 public userPk;
+    uint256 public recipient0Pk;
+    uint256 public recipient1Pk;
+    uint256 public recipient2Pk;
+
+    bytes32 public profileId;
 
     address public alloAdmin;
     address public owner;
     address public registry;
     address public treasury;
+    address public relayer;
 
     uint256 public percentFee;
     uint256 public baseFee;
 
+    /// @dev Send a transaction using the Biconomy Forwarder
+    function _sendWithRelayer(address _from, address _to, bytes memory _data, uint256 _userPk)
+        internal
+        returns (bool _success, bytes memory _returnData)
+    {
+        // User signs the request
+        IBiconomyForwarder.ERC20ForwardRequest memory req = IBiconomyForwarder.ERC20ForwardRequest({
+            from: _from,
+            to: _to,
+            token: address(0),
+            txGas: 500_000,
+            tokenGasPrice: 0,
+            batchId: 0,
+            batchNonce: 0,
+            deadline: block.timestamp + 1 days,
+            data: _data
+        });
+        bytes32 structHash = keccak256(
+            abi.encode(
+                IBiconomyForwarder(BICONOMY_FORWARDER).REQUEST_TYPEHASH(),
+                req.from,
+                req.to,
+                req.token,
+                req.txGas,
+                req.tokenGasPrice,
+                req.batchId,
+                IBiconomyForwarder(BICONOMY_FORWARDER).getNonce(req.from, req.batchId),
+                req.deadline,
+                keccak256(req.data)
+            )
+        );
+        bytes32 digest = DOMAIN_SEPARATOR.toTypedDataHash(structHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_userPk, digest);
+        bytes memory sig = abi.encodePacked(r, s, v);
+
+        // Relayer submits the request
+        vm.prank(relayer);
+        (_success, _returnData) = IBiconomyForwarder(BICONOMY_FORWARDER).executeEIP712(req, DOMAIN_SEPARATOR, sig);
+        assertTrue(_success);
+    }
+
     function setUp() public virtual {
         vm.createSelectFork(vm.rpcUrl("mainnet"), 20289932);
+
+        (userAddr, userPk) = makeAddrAndKey("user");
+        (recipient0Addr, recipient0Pk) = makeAddrAndKey("recipient0");
+        (recipient1Addr, recipient1Pk) = makeAddrAndKey("recipient1");
+        (recipient2Addr, recipient2Pk) = makeAddrAndKey("recipient2");
 
         // Get the current protocol variables
         // TODO: call admin() instead of reading storage 
@@ -44,5 +112,11 @@ abstract contract IntegrationBase is Test {
         // Initialize
         vm.prank(owner);
         IAllo(ALLO_PROXY).initialize(owner, registry, payable(treasury), percentFee, baseFee, BICONOMY_FORWARDER);
+
+        // Create a profile
+        vm.prank(userAddr);
+        profileId = IRegistry(registry).createProfile(
+            0, "Test Profile", Metadata({protocol: 1, pointer: ""}), userAddr, new address[](0)
+        );
     }
 }
