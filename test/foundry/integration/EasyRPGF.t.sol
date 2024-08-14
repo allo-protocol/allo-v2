c99ca97c4dfbc5398890a2e7261b8b70e69a75ec
0xAustrian <austrian@defi.sucks>
2024-08-14 14:49:58 -0300

    test: add IntegrationBase into IntegrationEasyRPGF

--- test/foundry/integration/EasyRPGF.t.sol
+++ test/foundry/integration/EasyRPGF.t.sol
@@ -1,69 +1,39 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.19;
 
-import {Test} from "forge-std/Test.sol";
-import {Allo} from "contracts/core/Allo.sol";
-import {Registry, Metadata} from "contracts/core/Registry.sol";
+import {IAllo} from "contracts/core/interfaces/IAllo.sol";
+import {Metadata} from "contracts/core/Registry.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {EasyRPGF} from "contracts/strategies/EasyRPGF.sol";
+import {IntegrationBase} from "./IntegrationBase.sol";
 
-contract IntegrationQVSimple is Test {
-    Allo public allo;
-    Registry public registry;
+contract IntegrationEasyRPGF is IntegrationBase {
+    IAllo public allo;
     EasyRPGF public strategy;
-
-    address public owner;
-    address public treasury;
-    address public profileOwner;
-    address public recipient0;
-    address public recipient1;
-    address public recipient2;
-
-    bytes32 public profileId;
 
     uint256 public poolId;
 
-    address public constant dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    function setUp() public override {
+        super.setUp();
 
-    function setUp() public {
-        vm.createSelectFork(vm.rpcUrl("mainnet"), 20289932);
+        allo = IAllo(ALLO_PROXY);
 
-        owner = makeAddr("owner");
-        treasury = makeAddr("treasury");
-        profileOwner = makeAddr("profileOwner");
-        recipient0 = makeAddr("recipient0");
-        recipient1 = makeAddr("recipient1");
-        recipient2 = makeAddr("recipient2");
-
-        // Deploying contracts
-        allo = new Allo();
-        registry = new Registry();
         strategy = new EasyRPGF(address(allo));
 
-        // Initialize contracts
-        allo.initialize(owner, address(registry), payable(treasury), 0, 0, address(1)); // NOTE: trusted forwarder is not used
-        registry.initialize(owner);
-
-        // Creating profile
-        vm.prank(profileOwner);
-        profileId = registry.createProfile(
-            0, "Test Profile", Metadata({protocol: 0, pointer: ""}), profileOwner, new address[](0)
-        );
-
         // Deal
-        deal(dai, profileOwner, 100000 ether);
-        vm.prank(profileOwner);
-        IERC20(dai).approve(address(allo), 100000 ether);
+        deal(DAI, userAddr, 100000 ether);
+        vm.prank(userAddr);
+        IERC20(DAI).approve(address(allo), 100000 ether);
 
         // Creating pool (and deploying strategy)
         address[] memory managers = new address[](1);
-        managers[0] = profileOwner;
-        vm.prank(profileOwner);
+        managers[0] = userAddr;
+        vm.prank(userAddr);
         poolId = allo.createPoolWithCustomStrategy(
             profileId,
             address(strategy),
             abi.encode(),
-            dai,
+            DAI,
             100000 ether,
             Metadata({protocol: 0, pointer: ""}),
             managers
@@ -78,9 +48,9 @@ contract IntegrationQVSimple is Test {
 
     function test_Distribute() public {
         address[] memory recipients = new address[](3);
-        recipients[0] = recipient0;
-        recipients[1] = recipient1;
-        recipients[2] = recipient2;
+        recipients[0] = recipient0Addr;
+        recipients[1] = recipient1Addr;
+        recipients[2] = recipient2Addr;
 
         uint256[] memory amounts = new uint256[](3);
         amounts[0] = 10 ether;
@@ -88,11 +58,11 @@ contract IntegrationQVSimple is Test {
         amounts[2] = 30 ether;
 
         vm.prank(address(allo));
-        strategy.distribute(recipients, abi.encode(amounts), profileOwner);
+        strategy.distribute(recipients, abi.encode(amounts), userAddr);
 
-        assertEq(IERC20(dai).balanceOf(recipient0), 10 ether);
-        assertEq(IERC20(dai).balanceOf(recipient1), 20 ether);
-        assertEq(IERC20(dai).balanceOf(recipient2), 30 ether);
+        assertEq(IERC20(DAI).balanceOf(recipient0Addr), 10 ether);
+        assertEq(IERC20(DAI).balanceOf(recipient1Addr), 20 ether);
+        assertEq(IERC20(DAI).balanceOf(recipient2Addr), 30 ether);
         assertEq(strategy.getPoolAmount(), 100000 ether - 60 ether);
     }
 }
