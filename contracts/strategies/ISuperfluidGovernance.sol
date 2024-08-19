fd739d2c37649e15eb2229df9b2786c0ea849b48
0xAustrian <austrian@defi.sucks>
2024-08-19 07:53:28 -0300

    test: wip, improvements on setUp

---
+++ contracts/strategies/ISuperfluidGovernance.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.19;
+
+interface ISuperfluidGovernance {
+    function owner() external view returns (address);
+    function setAppRegistrationKey(address host, address deployer, string memory registrationKey, uint256 expirationTs)
+        external;
+}
--- test/foundry/integration/SQFSuperfluid.t.sol
+++ test/foundry/integration/SQFSuperfluid.t.sol
@@ -1,44 +1,132 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.19;
 
-import {IAllo} from "contracts/core/interfaces/IAllo.sol";
-import {Metadata} from "contracts/core/Registry.sol";
-import {IntegrationBase} from "./IntegrationBase.sol";
+import {Test} from "forge-std/Test.sol";
+import {Allo, IAllo} from "contracts/core/Allo.sol";
 import {SQFSuperfluid} from "contracts/strategies/SQFSuperfluid.sol";
 import {IRecipientsExtension} from "contracts/extensions/interfaces/IRecipientsExtension.sol";
 import {IRecipientSuperAppFactory} from "contracts/strategies/interfaces/IRecipientSuperAppFactory.sol";
 import {RecipientSuperAppFactory} from "contracts/strategies/RecipientSuperAppFactory.sol";
+import {IOwnable} from "./IOwnable.sol";
+import {ITransparentUpgradeableProxy} from "./ITransparentUpgradeableProxy.sol";
+import {Metadata, IRegistry} from "contracts/core/Registry.sol";
+import {IGitcoinPassportDecoder} from "contracts/strategies/interfaces/IGitcoinPassportDecoder.sol";
+import {RecipientsExtension} from "contracts/extensions/contracts/RecipientsExtension.sol";
+import {ISuperfluidGovernance} from "contracts/strategies/ISuperfluidGovernance.sol";
 
-contract IntegrationSQFSuperfluid is IntegrationBase {
-    IAllo public allo;
+contract IntegrationSQFSuperfluid is Test {
     SQFSuperfluid public strategy;
     IRecipientSuperAppFactory public recipientSuperAppFactory;
 
-    address public constant PASSPORT_DECODER = address(0);
-    address public constant SUPERFLUID_HOST = address(0);
-    address public constant ALLOCATION_SUPER_TOKEN = address(0);
+    address public userAddr;
+    address public recipient0Addr;
+    address public recipient1Addr;
+    address public recipient2Addr;
 
-    uint256 public constant MIN_PASSPORT_SCORE = 0;
-    uint256 public constant INITIAL_SUPER_APP_BALANCE = 0;
+    uint256 public userPk;
+    uint256 public recipient0Pk;
+    uint256 public recipient1Pk;
+    uint256 public recipient2Pk;
+
+    bytes32 public profileId;
+
+    address public alloAdmin;
+    address public owner;
+    address public registry;
+    address public treasury;
+    address public relayer;
+
+    uint256 public percentFee;
+    uint256 public baseFee;
 
     uint256 public poolId;
 
-    function setUp() public override {
-        super.setUp();
+    address public constant ALLO_PROXY = 0x1133eA7Af70876e64665ecD07C0A0476d09465a1;
+    address public constant PASSPORT_DECODER = 0x5558D441779Eca04A329BcD6b47830D2C6607769;
+    address public constant SUPERFLUID_HOST = 0x567c4B141ED61923967cA25Ef4906C8781069a10;
+    address public constant ALLOCATION_SUPER_TOKEN = 0x7d342726B69C28D942ad8BfE6Ac81b972349d524;
+    address public constant POOL_SUPER_TOKEN = 0x4ac8bD1bDaE47beeF2D1c6Aa62229509b962Aa0d;
+    address public constant SUPERFLUID_GOV = 0x0170FFCC75d178d426EBad5b1a31451d00Ddbd0D;
 
-        allo = IAllo(ALLO_PROXY);
+    uint256 public constant MIN_PASSPORT_SCORE = 30000;
+    uint256 public constant INITIAL_SUPER_APP_BALANCE = 10000000000;
 
+    // TODO: import this function from utils
+    function _getApplicationStatus(address[] memory _recipientIds, uint256[] memory _statuses, address _strategy)
+        internal
+        view
+        returns (IRecipientsExtension.ApplicationStatus memory)
+    {
+        IRecipientsExtension.Recipient memory recipient =
+            RecipientsExtension(payable(_strategy)).getRecipient(_recipientIds[0]);
+        uint256 recipientIndex = uint256(recipient.statusIndex) - 1;
+
+        uint256 rowIndex = recipientIndex / 64;
+        uint256 statusRow = RecipientsExtension(payable(_strategy)).statusesBitMap(rowIndex);
+        for (uint256 i = 0; i < _recipientIds.length; i++) {
+            recipient = RecipientsExtension(payable(_strategy)).getRecipient(_recipientIds[i]);
+            recipientIndex = uint256(recipient.statusIndex) - 1;
+
+            require(rowIndex == recipientIndex / 64, "_recipientIds belong to different rows");
+            uint256 colIndex = (recipientIndex % 64) * 4;
+            uint256 newRow = statusRow & ~(15 << colIndex);
+            statusRow = newRow | (_statuses[i] << colIndex);
+        }
+
+        return IRecipientsExtension.ApplicationStatus({index: rowIndex, statusRow: statusRow});
+    }
+
+    function setUp() public {
+        vm.createSelectFork(vm.rpcUrl("optimism"), 123864457);
+
+        (userAddr, userPk) = makeAddrAndKey("user");
+        (recipient0Addr, recipient0Pk) = makeAddrAndKey("recipient0");
+        (recipient1Addr, recipient1Pk) = makeAddrAndKey("recipient1");
+        (recipient2Addr, recipient2Pk) = makeAddrAndKey("recipient2");
+
+        // Get the current protocol variables
+        alloAdmin = address(
+            uint160(uint256(vm.load(ALLO_PROXY, 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103)))
+        );
+        owner = IOwnable(ALLO_PROXY).owner();
+        registry = address(IAllo(ALLO_PROXY).getRegistry());
+        treasury = address(IAllo(ALLO_PROXY).getTreasury());
+        percentFee = IAllo(ALLO_PROXY).getPercentFee();
+        baseFee = IAllo(ALLO_PROXY).getBaseFee();
+
+        // Deploy the implementation
+        vm.prank(alloAdmin);
+        address implementation = address(new Allo());
+
+        // Deploy the update
+        vm.prank(alloAdmin);
+        ITransparentUpgradeableProxy(ALLO_PROXY).upgradeTo(implementation);
+
+        // Initialize
+        vm.prank(owner);
+        IAllo(ALLO_PROXY).initialize(owner, registry, payable(treasury), percentFee, baseFee, address(1));
+
+        // Create a profile
+        vm.prank(userAddr);
+        profileId = IRegistry(registry).createProfile(
+            0, "Test Profile", Metadata({protocol: 1, pointer: ""}), userAddr, new address[](0)
+        );
+
+        // Deploy strategy and recipient super app factory
         strategy = new SQFSuperfluid(ALLO_PROXY);
         recipientSuperAppFactory = new RecipientSuperAppFactory();
 
-        // Deal 130k DAI to the user
-        deal(DAI, userAddr, 130_000 ether);
+        // Authorize factory
+        vm.prank(ISuperfluidGovernance(SUPERFLUID_GOV).owner());
+        ISuperfluidGovernance(SUPERFLUID_GOV).setAppRegistrationKey(
+            SUPERFLUID_HOST, address(recipientSuperAppFactory), "k1", block.timestamp + 4 weeks
+        );
 
         // Creating pool (and deploying strategy)
         address[] memory managers = new address[](1);
         managers[0] = userAddr;
         vm.prank(userAddr);
-        poolId = allo.createPoolWithCustomStrategy(
+        poolId = IAllo(ALLO_PROXY).createPoolWithCustomStrategy(
             profileId,
             address(strategy),
             abi.encode(
@@ -58,18 +146,77 @@ contract IntegrationSQFSuperfluid is IntegrationBase {
                     initialSuperAppBalance: INITIAL_SUPER_APP_BALANCE
                 })
             ),
-            DAI,
-            100000 ether,
+            POOL_SUPER_TOKEN,
+            0,
             Metadata({protocol: 0, pointer: ""}),
             managers
         );
+
+        // Deal initial balance * recipients of DAIx to the strategy
+        deal(ALLOCATION_SUPER_TOKEN, address(strategy), INITIAL_SUPER_APP_BALANCE * 2);
+
+        // Mock score in passport
+        vm.mockCall(
+            PASSPORT_DECODER,
+            abi.encodeWithSelector(IGitcoinPassportDecoder.getScore.selector, userAddr),
+            abi.encode(MIN_PASSPORT_SCORE)
+        );
+
+        // Register recipients
+        vm.startPrank(ALLO_PROXY);
+        address[] memory recipients = new address[](1);
+        bytes[] memory data = new bytes[](1);
+
+        recipients[0] = recipient0Addr;
+        data[0] = abi.encode(address(0), Metadata({protocol: 0, pointer: ""}), bytes(""));
+        strategy.register(recipients, abi.encode(data), recipient0Addr);
+
+        recipients[0] = recipient1Addr;
+        data[0] = abi.encode(address(0), Metadata({protocol: 0, pointer: ""}), bytes(""));
+        strategy.register(recipients, abi.encode(data), recipient1Addr);
+
+        recipients[0] = recipient2Addr;
+        data[0] = abi.encode(address(0), Metadata({protocol: 0, pointer: ""}), bytes(""));
+        strategy.register(recipients, abi.encode(data), recipient2Addr);
+        vm.stopPrank();
+
+        // Review recipients
+        address[] memory _recipientIds = new address[](2);
+        _recipientIds[0] = recipient0Addr;
+        _recipientIds[1] = recipient1Addr;
+
+        uint256[] memory _newStatuses = new uint256[](3);
+        _newStatuses[0] = uint256(IRecipientsExtension.Status.Accepted);
+        _newStatuses[1] = uint256(IRecipientsExtension.Status.Accepted);
+
+        IRecipientsExtension.ApplicationStatus[] memory statuses = new IRecipientsExtension.ApplicationStatus[](1);
+        statuses[0] = _getApplicationStatus(_recipientIds, _newStatuses, address(strategy));
+
+        uint256 recipientsCounter = strategy.recipientsCounter();
+
+        vm.prank(userAddr);
+        strategy.reviewRecipients(statuses, recipientsCounter);
     }
 
     function test_Allocate() public {
-        //
+        vm.skip(true);
+
+        address[] memory recipients = new address[](2);
+        recipients[0] = recipient0Addr;
+        recipients[1] = recipient1Addr;
+
+        // wei of DAIx per second
+        int96[] memory flowRates = new int96[](2);
+        flowRates[0] = 0.01 ether;
+        flowRates[1] = 0.02 ether;
+
+        // TODO: call the allocate function from Allo.sol after PR #641
+        vm.prank(ALLO_PROXY);
+        strategy.allocate(recipients, new uint256[](0), abi.encode(flowRates), userAddr);
     }
 
     function test_Distribute() public {
         //
+        vm.skip(true);
     }
 }
