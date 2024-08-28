3b6bba3ea8c87e03fb7bf6c535e215f070220a03
0xAustrian <austrian@defi.sucks>
2024-08-28 10:19:04 -0300

    feat: bump sol version

--- contracts/extensions/contracts/AllocationExtension.sol
+++ contracts/extensions/contracts/AllocationExtension.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import {IAllocationExtension} from "contracts/extensions/interfaces/IAllocationExtension.sol";
 import {CoreBaseStrategy} from "contracts/strategies/CoreBaseStrategy.sol";
