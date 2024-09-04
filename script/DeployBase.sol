7fe233337fb594aca13252cce4b47535f71ceb1d
0xAustrian <austrian@defi.sucks>
2024-09-04 10:39:48 -0300

    create DeployBase.sol

---
+++ script/DeployBase.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+
+abstract contract DeployBase is Script {
+    /// @notice Deployment parameters for each chain
+    mapping(uint256 _chainId => bytes _deploymentData) internal _deploymentParams;
+
+    function run() public {
+        bytes memory _params = _deploymentParams[block.chainid];
+
+        vm.startBroadcast();
+        address _contract = _deploy(block.chainid, _params);
+        vm.stopBroadcast();
+        
+        console.log("Deployed contract at address: %s", _contract);
+    }
+
+    function _deploy(uint256 _chainId, bytes memory _data) internal virtual returns (address _contract) {}
+}
